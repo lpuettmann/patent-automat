588dce92415b9babdee4abb8c4d882559d7e2e86
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-02-02 17:15:11 +0100

    Looked for patents by keywords PATN and WKU. There is a discrepancy of 1 which comes from word PATNO on line06085.

--- analyze_patent_text.m
+++ analyze_patent_text.m
@@ -11,8 +11,8 @@ tic
 %% Define keyword to look for
 % ========================================================================
 % find_str = 'automatization';
-% find_str = 'automated';
-find_str = 'automat.'; % use a dot as a place holder
+find_str = 'automated';
+% find_str = 'automat.'; % use a dot as a place holder
 % find_str = 'autoMATEd';
 % find_str = '4354706'
 
@@ -27,40 +27,64 @@ end
 % ========================================================================
 file_str = fileread('pftaps19821019_wk42.txt');
 
+% Define new search corpus as we might change some things about this
 search_corpus = file_str; 
 
 
-%% Show number of patents in the week
+%% Count number of patents in a given week
 % ========================================================================
-[ix_find, nr_patents_week] = count_nr_string_appearance(file_str, ...
+
+% Count number of appearances of 'PATN'
+% ------------------------------------------------------------------------
+[ix_PATN_find, nr_PATN_week] = count_nr_string_appearance(file_str, ...
     'PATN');
 
-fprintf('Number of patents in week: %d.\n', nr_patents_week)
-disp('---------------------------------------------------------------')
 
+% Pre-define empty cell array to store patent WKU numbers (based on finding) PATN
+patent_number_PATN = repmat({''}, nr_PATN_week, 1);
 
-% 
-% choose_patn_ix = 1234;
-% 
-% patent_number_patn = file_str(ix_find(choose_patn_ix) + 11 : ...
-%     ix_find(choose_patn_ix) + 19)
-
-% last one: 043554237
-
-[ix_find, nr_patents_week] = count_nr_string_appearance(file_str, ...
-    'WKU');
-
-
-% Pre-define empty cell array to store patent WKU numbers
-patent_number_WKU = repmat({''}, nr_patents_week, 1);
-
-for ix_choose_wku=1:nr_patents_week
-    patent_number_WKU{ix_choose_wku} = file_str(ix_find(ix_choose_wku) + 5 : ...
-        ix_find(ix_choose_wku) + 13);
+for i=1:nr_PATN_week
+    patent_number_PATN{i} = file_str(ix_PATN_find(i) + 11 : ix_PATN_find(i) + 20);
 end
 
 
-% Plausibility check: test if there are any spaces in WKU numbers
+% Count number of appearances of 'WKU' (patent numbers)
+% ------------------------------------------------------------------------
+[ix_find, nr_WKU_week] = count_nr_string_appearance(file_str, ...
+    'WKU');
+
+fprintf('Number "PATN": %d, number "WKU": %d. \n', nr_PATN_week, nr_WKU_week)
+disp('---------------------------------------------------------------')
+
+
+% Pre-define empty cell array to store patent WKU numbers
+patent_number_WKU = repmat({''}, nr_WKU_week, 1);
+
+for i=1:nr_WKU_week
+    patent_number_WKU{i} = file_str(ix_find(i) + 5 : ix_find(i) + 13);
+end
+
+
+%% Run plausibility check
+% ========================================================================
+% Check which elements in patent_number_PATN and patent_number_WKU differ
+for i =1:min(nr_PATN_week, nr_WKU_week)
+    pick_patn = patent_number_PATN{i};
+    pick_patn = cellstr(pick_patn);
+    
+    pick_wku = patent_number_WKU{i};
+    
+    check_equal_patn_wku(i) = strcmp(pick_patn, pick_wku);
+end
+
+notequal_patn_wku = find(~check_equal_patn_wku)';
+
+if sum(check_equal_patn_wku) > 0
+    warning('We are finding different number of PATN and WKU numbers.')
+end
+
+
+% Test if there are any spaces in WKU numbers
 test_contains_space = strfind(patent_number_WKU, ' ');
 show_ix_contains_space = find(~cellfun(@isempty,test_contains_space));
 if not(isempty(show_ix_contains_space))
@@ -68,9 +92,12 @@ if not(isempty(show_ix_contains_space))
 end
 
 
-
-break
-
+% Test if all WKU numbers are 9 digits long
+test_is9long = cellfun(@length, patent_number_WKU);
+test_vector_nines = repmat(9, nr_WKU_week, 1);
+if min(test_is9long == test_vector_nines) < 1
+    warning('Not all patent WKU numbers are 9 characters long')
+end
 
 
 %% Search
@@ -91,6 +118,7 @@ nr_find = length(ix_find);
 %     explore_ix_found, show_char, nr_show_matches, match_disp_choice)
 
 
+
 %% Display findings
 % ========================================================================
 fprintf('Number of times appearance of string: %d.\n', nr_find)
@@ -100,6 +128,12 @@ disp('---------------------------------------------------------------')
 
 %% Find number of patent belonging to each hit
 % ========================================================================
+% 
+% ix_matches = 1
+% 
+% match_position = ix_find(ix_matches)
+
+
 
 
 
