ffc106cfee543b91d51da4bc984bf40ee49348b8
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-02-04 15:43:35 +0100

    Loop over years. Save extracted information to .mat file.

--- analyze_patent_text.m
+++ analyze_patent_text.m
@@ -13,185 +13,200 @@ find_str = 'automat';
 
 %% Choose time period to analyze
 % ========================================================================
-year = 2001;
-week_start = 52; % default: 42
-week_end = 52; % this can be the same as week_start
-% 53 weeks: 1980, 1985, 1991, 1996
 
-build_data_path = horzcat('.\data\', num2str(year));
+for ix_year = 1980:1980
+    year = ix_year;
+    week_start = 52;
+    week_end = 52; % this can be the same as week_start
 
-addpath('functions');
-addpath('data');
-addpath(build_data_path);
-
-
-%% Get names of files
-% ========================================================================
-liststruct = dir(build_data_path);
-filenames = {liststruct.name};
-filenames = filenames(3:end)'; % truncate first elements . and ..
-
-
-%% ITERATE THROUGH FILES WITH WEEK PATENT DATA
-% ========================================================================
-disp('~~~ enter loop: ~~~')
-
-for ix_week = week_start:week_end
-    choose_file_open = filenames{ix_week};
-
-    % Load the patent text
-    % --------------------------------------------------------------------
-    unique_file_identifier = fopen(choose_file_open, 'r');   
-    
-    if unique_file_identifier == -1
-        warning('Matlab cannot open the file')
-    end
-        
-    open_file_aux = textscan(unique_file_identifier, '%s', ...
-        'delimiter', '\n');
-    file_str = open_file_aux{1,1};
-
-    
-    % Define new search corpus as we might change some things about this
-    search_corpus = file_str; 
-
-
-    % Eliminate the name section from the search corpus
-    % --------------------------------------------------------------------
-    ix_find_NAM = strfind(file_str,'NAM');
-    show_row_NAM = find(~cellfun(@isempty,ix_find_NAM));
-
-    search_corpus(show_row_NAM) = []; % delete rows with NAN
-
-    % Test if we get the right number of rows
-    if length(file_str)-length(search_corpus) ~= length(show_row_NAM)
-        warning(['Are you sure you deleted the right columns with ', ...
-            'NAM in them?'])
+    % 53 weeks: 1980, 1985, 1991, 1996
+    if year == 1980 | year == 1985 | ear == 1991 | year == 1996
+        week_end = 53;
     end
 
 
-    %% Count number of patents in a given week
-    % --------------------------------------------------------------------
-    
-    if year == 2001 % special case: problem with 80 numel text file
-        search_corpus_trunc4 = search_corpus;
-        disp('Year 2001, special case')
-        for i=1:length(search_corpus_trunc4)
-            if numel(search_corpus_trunc4{i}) > 4
-                row_shorten = search_corpus_trunc4{i};
-                search_corpus_trunc4{i} = row_shorten(1:4);
+    build_data_path = horzcat('.\data\', num2str(year));
+
+    addpath('functions');
+    addpath('data');
+    addpath(build_data_path);
+
+
+    %% Get names of files
+    % ========================================================================
+    liststruct = dir(build_data_path);
+    filenames = {liststruct.name};
+    filenames = filenames(3:end)'; % truncate first elements . and ..
+
+
+    %% ITERATE THROUGH FILES WITH WEEK PATENT DATA
+    % ========================================================================
+    fprintf('* Enter loop for year %d\n', year)
+
+    for ix_week = week_start:week_end
+        choose_file_open = filenames{ix_week};
+
+        % Load the patent text
+        % --------------------------------------------------------------------
+        unique_file_identifier = fopen(choose_file_open, 'r');   
+
+        if unique_file_identifier == -1
+            warning('Matlab cannot open the file')
+        end
+
+        open_file_aux = textscan(unique_file_identifier, '%s', ...
+            'delimiter', '\n');
+        file_str = open_file_aux{1,1};
+
+
+        % Define new search corpus as we might change some things about this
+        search_corpus = file_str; 
+
+
+        % Eliminate the name section from the search corpus
+        % --------------------------------------------------------------------
+        ix_find_NAM = strfind(file_str,'NAM');
+        show_row_NAM = find(~cellfun(@isempty,ix_find_NAM));
+
+        search_corpus(show_row_NAM) = []; % delete rows with NAN
+
+        % Test if we get the right number of rows
+        if length(file_str)-length(search_corpus) ~= length(show_row_NAM)
+            warning(['Are you sure you deleted the right columns with ', ...
+                'NAM in them?'])
+        end
+
+
+        %% Count number of patents in a given week
+        % --------------------------------------------------------------------
+
+        if year == 2001 % special case: problem with 80 numel text file
+            search_corpus_trunc4 = search_corpus;
+            disp('Year 2001, special case')
+            for i=1:length(search_corpus_trunc4)
+                if numel(search_corpus_trunc4{i}) > 4
+                    row_shorten = search_corpus_trunc4{i};
+                    search_corpus_trunc4{i} = row_shorten(1:4);
+                end
             end
-        end
 
-        [indic_find, nr_patents, ix_find] = count_nr_patents(...
-            search_corpus_trunc4, 'PATN');
-    else
-        [indic_find, nr_patents, ix_find] = count_nr_patents(...
-            search_corpus, 'PATN'); 
-    end
-
-    % Test: did not find patents
-    if nr_patents < 100
-        warning(['The number of patents (= %d) is implausibly small'], ...
-            nr_patents)
-    end    
-
-    % Pre-define empty cell array to store patent WKU numbers (based on finding) PATN
-    patent_number = repmat({''}, nr_patents, 1);
-
-    for i=1:nr_patents
-         wku_line = search_corpus(ix_find(i)+1, :);
-         wku_line = wku_line{1};
-         patent_number{i} = wku_line(6:14);
-    end
-    
-    
-    % Test if there are any spaces in WKU numbers
-    test_contains_space = strfind(patent_number, ' ');
-    show_ix_contains_space = find(~cellfun(@isempty, test_contains_space));
-    if not(isempty(show_ix_contains_space))
-        warning('There is a space in the patent WKU numbers')
-    end
-
-
-    % Test if all WKU numbers are 9 digits long
-    test_is9long = cellfun(@length, patent_number);
-    test_vector_nines = repmat(9, nr_patents, 1);
-    if min(test_is9long == test_vector_nines) < 1
-        warning('Not all patent WKU numbers are 9 characters long')
-    end
-
-
-    % Extract patent text
-    % --------------------------------------------------------------------
-    nr_keyword_appear = patent_number;
-    
-    % Get empty cells next to the WKU patent numbers. This will be filled
-    % up with the number of matches for each patent
-    nr_keyword_appear{1,2} = []; 
-
-    % Insert the current year for later reference
-    nr_keyword_appear = [nr_keyword_appear, ...
-        num2cell(repmat(year, nr_patents, 1))];
-    
-    % Insert the current week for later reference
-    nr_keyword_appear = [nr_keyword_appear, ...
-        num2cell(repmat(ix_week, nr_patents, 1))];
-    
-    
-    for ix_patent=1:nr_patents
-
-        % Get start and end of patent text
-        % ----------------------------------------------------------------
-        start_text_corpus = ix_find(ix_patent);
-
-        if ix_patent < nr_patents
-            end_text_corpus = ix_find(ix_patent+1)-1;
+            [indic_find, nr_patents, ix_find] = count_nr_patents(...
+                search_corpus_trunc4, 'PATN');
         else
-            end_text_corpus = length(search_corpus);
+            [indic_find, nr_patents, ix_find] = count_nr_patents(...
+                search_corpus, 'PATN'); 
         end
 
-        patent_text_corpus = search_corpus(start_text_corpus:...
-            end_text_corpus, :);
+        % Test: did not find patents
+        if nr_patents < 100
+            warning(['The number of patents (= %d) is implausibly small'], ...
+                nr_patents)
+        end    
 
-        % Search
-        % ----------------------------------------------------------------
-        ix_keyword_find = regexpi(patent_text_corpus, find_str);
-        ix_keyword_find = ix_keyword_find(~cellfun('isempty', ...
-            ix_keyword_find));
-        nr_keyword_find = length(ix_keyword_find);
+        % Pre-define empty cell array to store patent WKU numbers (based on finding) PATN
+        patent_number = repmat({''}, nr_patents, 1);
 
-        % Save
-        % ----------------------------------------------------------------
-        nr_keyword_appear{ix_patent, 2} = nr_keyword_find;
+        for i=1:nr_patents
+             wku_line = search_corpus(ix_find(i)+1, :);
+             wku_line = wku_line{1};
+             patent_number{i} = wku_line(6:14);
+        end
+
+
+        % Test if there are any spaces in WKU numbers
+        test_contains_space = strfind(patent_number, ' ');
+        show_ix_contains_space = find(~cellfun(@isempty, test_contains_space));
+        if not(isempty(show_ix_contains_space))
+            warning('There is a space in the patent WKU numbers')
+        end
+
+
+        % Test if all WKU numbers are 9 digits long
+        test_is9long = cellfun(@length, patent_number);
+        test_vector_nines = repmat(9, nr_patents, 1);
+        if min(test_is9long == test_vector_nines) < 1
+            warning('Not all patent WKU numbers are 9 characters long')
+        end
+
+
+        % Extract patent text
+        % --------------------------------------------------------------------
+        nr_keyword_appear = patent_number;
+
+        % Get empty cells next to the WKU patent numbers. This will be filled
+        % up with the number of matches for each patent
+        nr_keyword_appear{1,2} = []; 
+
+        % Insert the current year for later reference
+        nr_keyword_appear = [nr_keyword_appear, ...
+            num2cell(repmat(year, nr_patents, 1))];
+
+        % Insert the current week for later reference
+        nr_keyword_appear = [nr_keyword_appear, ...
+            num2cell(repmat(ix_week, nr_patents, 1))];
+
+
+        for ix_patent=1:nr_patents
+
+            % Get start and end of patent text
+            % ----------------------------------------------------------------
+            start_text_corpus = ix_find(ix_patent);
+
+            if ix_patent < nr_patents
+                end_text_corpus = ix_find(ix_patent+1)-1;
+            else
+                end_text_corpus = length(search_corpus);
+            end
+
+            patent_text_corpus = search_corpus(start_text_corpus:...
+                end_text_corpus, :);
+
+            % Search
+            % ----------------------------------------------------------------
+            ix_keyword_find = regexpi(patent_text_corpus, find_str);
+            ix_keyword_find = ix_keyword_find(~cellfun('isempty', ...
+                ix_keyword_find));
+            nr_keyword_find = length(ix_keyword_find);
+
+            % Save
+            % ----------------------------------------------------------------
+            nr_keyword_appear{ix_patent, 2} = nr_keyword_find;
+        end
+
+        % Save information for all weeks
+        % -------------------------------------------------------------------
+        if ix_week == week_start % first iteration: have to newly define this variable
+            patent_keyword_appear = repmat({''}, 1, ...
+                size(nr_keyword_appear, 2));
+        end        
+
+        patent_keyword_appear = [patent_keyword_appear;
+                                 nr_keyword_appear];
+
+        if ix_week == week_end % last iteration: delete first row
+            patent_keyword_appear(1,:) = [];
+        end 
+
+        fprintf('Week finished: %d/%d\n', ix_week, week_end)
     end
+
     
-    % Save information for all weeks
-    % -------------------------------------------------------------------
-    if ix_week == week_start % first iteration: have to newly define this variable
-        patent_keyword_appear = repmat({''}, 1, ...
-            size(nr_keyword_appear, 2));
-    end        
-        
-    patent_keyword_appear = [patent_keyword_appear;
-                             nr_keyword_appear];
-    
-    if ix_week == week_end % last iteration: delete first row
-        patent_keyword_appear(1,:) = [];
-    end 
-    
-    disp(ix_week)
+    %% Save
+    % ========================================================================
+    save_name = horzcat('patent_keyword_appear_', num2str(year), '.mat');
+    save(save_name, 'patent_keyword_appear')
+
+
+    disp('---------------------------------------------------------------')
+    fprintf('Year %d finished, time: %d seconds \n', year, round(toc))
+    disp('---------------------------------------------------------------')
+
+
 end
-disp('---------------------------------------------------------------')
-disp('+++ ...FINISHED LOOP! +++')
 
 
-%% Save
-% ========================================================================
-% save_name = horzcat('patent_keyword_appear_', num2str(year), '.mat');
-% save(save_name, 'patent_keyword_appear')
-% 
-% 
+
+
 % %% Display findings
 % % ========================================================================
 % nr_keyword_per_patent = cell2mat(patent_keyword_appear(:, 2));
@@ -281,5 +296,4 @@ disp('+++ ...FINISHED LOOP! +++')
 
 %% End
 % ======================================================================
-toc
 disp('*** end ***')
