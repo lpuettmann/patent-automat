d9c40d7485ef7f6b0687e5ab02d2c8984a5c8620
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-02-12 19:17:45 +0100

    Complete patent index / delete old functions
    The script find_patents now saves the WKU patent numbers and the index positions in the weekly files in a cell array for each week. These are then saved in
    a cell array for the whole year pat_ix.

--- find_patents.m
+++ find_patents.m
@@ -1,18 +1,25 @@
-clc
-clear all
 close all
+clear all
+clc
 
 
-find_str = 'automat'; % delete this
 
-
-addpath('functions');
-% addpath('data');
-
-%% Choose time period to analyze
+%% Add path to functions
 % ========================================================================
+addpath('functions');
 
-for ix_year = 1976:1979
+
+
+%% Set some inputs
+% ========================================================================
+year_start = 1976;
+year_end = 1978;
+
+
+
+%% Go
+% ========================================================================
+for ix_year = year_start:year_end
     tic
     
     year = ix_year;
@@ -24,28 +31,28 @@ for ix_year = 1976:1979
     else
         week_end = 52; 
     end
+    
+    week_end = 4;
 
     build_data_path = horzcat('.\data\', num2str(year));
-
     addpath(build_data_path);
 
 
-    %% Get names of files
-    % ========================================================================
+    % Get names of files
+    % -------------------------------------------------------------------
     liststruct = dir(build_data_path);
     filenames = {liststruct.name};
     filenames = filenames(3:end)'; % truncate first elements . and ..
 
 
-    %% ITERATE THROUGH FILES WITH WEEK PATENT DATA
-    % ========================================================================
-    fprintf('* Enter loop for year %d\n', year)
+    % ITERATE THROUGH FILES WITH WEEK PATENT DATA
+    % -------------------------------------------------------------------
+    fprintf('Enter loop for year %d:\n', year)
 
     for ix_week = week_start:week_end
         choose_file_open = filenames{ix_week};
 
         % Load the patent text
-        % -------------------------------------------------------------------
         unique_file_identifier = fopen(choose_file_open, 'r');   
 
         if unique_file_identifier == -1
@@ -75,7 +82,7 @@ for ix_year = 1976:1979
         end
 
 
-        %% Count number of patents in a given week
+        % Count number of patents in a given week
         % --------------------------------------------------------------------
 
         if year == 2001 % special case: problem with 80 numel text file
@@ -178,83 +185,27 @@ for ix_year = 1976:1979
             warning('Not all patent WKU numbers are 9 characters long')
         end
 
-
-        % Extract patent text
-        % --------------------------------------------------------------------
-        nr_keyword_appear = patent_number;
-
-        % Get empty cells next to the WKU patent numbers. 
-        nr_keyword_appear{1,2} = []; % column for keyword matches
-        nr_keyword_appear{1,3} = []; % column for OCL classifications
-
-        % Insert the current year for later reference
-        nr_keyword_appear = [nr_keyword_appear, ...
-            num2cell(repmat(year, nr_patents, 1))];
-
-        % Insert the current week for later reference
-        nr_keyword_appear = [nr_keyword_appear, ...
-            num2cell(repmat(ix_week, nr_patents, 1))];
-
-
-        for ix_patent=1:nr_patents
-
-            % Get start and end of patent text
-            % ----------------------------------------------------------------
-            start_text_corpus = ix_find(ix_patent);
-
-            if ix_patent < nr_patents
-                end_text_corpus = ix_find(ix_patent+1)-1;
-            else
-                end_text_corpus = length(search_corpus);
-            end
-
-            patent_text_corpus = search_corpus(start_text_corpus:...
-                end_text_corpus, :);
-
-            % Search for keyword
-            % ----------------------------------------------------------------
-            ix_keyword_find = regexpi(patent_text_corpus, find_str);
-            ix_keyword_find = ix_keyword_find(~cellfun('isempty', ...
-                ix_keyword_find));
-            nr_keyword_find = length(ix_keyword_find);
-            
-            % Look up OCL (tech classification)
-            % ----------------------------------------------------------------
-            ix_find_OCL = strfind(patent_text_corpus, 'OCL');
-            all_OCL_matches = find(~cellfun(@isempty,ix_find_OCL));
-            row_OCL_class = patent_text_corpus{all_OCL_matches(1)};
-            patent_OCL_class = row_OCL_class(5:numel(row_OCL_class));
-            
-            % Stack weekly information underneath
-            % ----------------------------------------------------------------
-            nr_keyword_appear{ix_patent, 2} = nr_keyword_find;
-            nr_keyword_appear{ix_patent, 3} = patent_OCL_class;
-        end
-
-        % Save information for all weeks
+        
+        % Test number of WKU numbers equal number of index positions
+        if length(patent_number) ~= length(ix_find)
+            warning('Should be the same.')
+        end            
+        
+        % Define patent index. It consists of the patent's WKU number 
+        % and its index position in the file. Save information for week in cell array
         % -------------------------------------------------------------------
-        if ix_week == week_start % first iteration: have to newly define this variable
-            patent_keyword_appear = repmat({''}, 1, ...
-                size(nr_keyword_appear, 2));
-        end        
-
-        patent_keyword_appear = [patent_keyword_appear;
-                                 nr_keyword_appear];
-
-        if ix_week == week_end % last iteration: delete first row
-            patent_keyword_appear(1,:) = [];
-        end 
-
+        pat_ix_weekly{ix_week, 1} = patent_number;
+        pat_ix_weekly{ix_week, 2} = ix_find;
+        
         fprintf('Week finished: %d/%d.\n', ix_week, week_end)
     end
+    
+    pat_ix{ix_year - year_start + 1, 1} = pat_ix_weekly;
 
-
-
+    
     disp('---------------------------------------------------------------')
     fprintf('Year %d finished, time: %d seconds \n', year, round(toc))
     disp('---------------------------------------------------------------')
-
-
 end
 
 
