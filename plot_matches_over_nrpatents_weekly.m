40542fbaaa0b94100404a2688a6117b92ee3e891
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-02-12 17:50:20 +0100

    ..

--- plot_matches_over_nrpatents_weekly.m
+++ plot_matches_over_nrpatents_weekly.m
@@ -29,21 +29,26 @@ matches_per_patent_weekly = allyear_total_matches_week ./ ...
 
 
 %% Make time series plot of matches per week
-my_xaxis_labels = {1976; ''; ''; ''; 1980; ''; ''; ''; ''; 1985; ''; ''; ''; ''; 1990; ''; ''; ''; ''; 1995; ''; ''; ''; ''; ''; 2001;};
+my_xaxis_labels = {1976; ''; ''; ''; 1980; ''; ''; ''; ''; 1985; ''; ''; ''; ''; 1990; ''; ''; ''; ''; 1995; ''; ''; ''; ''; 2000; ''; ''};
 
 color2_pick = [0.000,0.639,0.561];
 my_gray = [0.806, 0.806, 0.806];
+my_dark_gray = [0.3, 0.3, 0.3];
 
+
+rescale_markers = allyear_total_matches_week ./ 50;
 
 
 figureHandle = figure;
 hold on
-scatter(1:length(matches_per_patent_weekly), matches_per_patent_weekly, ...
-    'Marker', 'o', 'MarkerEdgeColor', color2_pick)
+h_scatter = scatter(1:length(matches_per_patent_weekly), ...
+    matches_per_patent_weekly, rescale_markers, ...
+    'Marker', 'o', 'MarkerEdgeColor', color2_pick);
 
 h_trend = plot(1:length(matches_per_patent_weekly), plot_trend, ...
-    'Color', color2_pick, 'Linewidth', 3);
+    'Color', color2_pick, 'Linewidth', 1.6);
 
+uistack(h_scatter, 'top');
 uistack(h_trend, 'bottom');
 
 set(gca,'FontSize',11) % change default font size of axis labels
@@ -55,6 +60,7 @@ title(title_phrase, 'FontSize', 11, ...
     'Position', [0.365 1.1], ...
     'HorizontalAlignment', 'right')
 
+% set(gca, 'XColor', my_dark_gray, 'YColor', my_dark_gray)
 set(gca,'TickDir','out')  
 box off
 set(gcf, 'Color', 'w');
@@ -67,7 +73,7 @@ set(gca, 'xticklabel', my_xaxis_labels);
 
 % Reposition the figure
 % -----------------------------------------------------------------------
-set(gcf, 'Position', [100 200 800 500]) % in vector: left bottom width height
+set(gcf, 'Position', [100 200 800 600]) % in vector: left bottom width height
 
 set(figureHandle, 'Units', 'Inches');
 pos = get(figureHandle, 'Position');
