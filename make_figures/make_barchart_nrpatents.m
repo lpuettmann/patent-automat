2c157935c3f56fdd4826c308b84f0701e68b57cd
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-02-24 12:22:26 +0100

    Split figure making
    Keep the original two by two subplots summary stats over time and make new script with bar chart for number of identified patents.

---
+++ make_figures/make_barchart_nrpatents.m
@@ -0,0 +1,95 @@
+close all
+clear all
+clc
+
+% Define parameters
+year_start = 1976;
+year_end = 2014;
+
+
+
+
+%% Load summary data
+build_load_filename = horzcat('patent_match_summary_', num2str(year_start), ...
+    '-', num2str(year_end), '.mat');
+load(build_load_filename)
+
+
+%% Print some summaries
+fprintf('Total number of identified patents all years: %d.\n', ...
+    sum(patent_match_summary.nr_patents_yr))
+
+
+
+%% Some settings for the plots
+plot_time = year_start:year_end;
+color1_pick = [0.7900, 0.3800, 0.500];
+color2_pick = [0.000,0.639,0.561];
+color3_pick = [0.890,0.412,0.525];
+color4_pick = [0.890,0.412,0.525];
+
+
+my_gray = [0.6706, 0.6706, 0.6706];
+
+
+figureHandle = figure;
+
+% Plot total number of identified patents per year
+bar(plot_time, patent_match_summary.nr_patents_yr, 'FaceColor', ...
+    color4_pick, 'EdgeColor', 'k')
+hold on
+bar(plot_time, patent_match_summary.nr_distinct_patents_hits, ...
+    'FaceColor', color1_pick, 'EdgeColor', 'k')
+hold off
+
+set(gca,'FontSize',12) % change default font size of axis labels
+box off
+set(gca,'TickDir','out'); 
+ylim([0 400000])
+set(gca, 'YTickLabel', num2str(get(gca, 'YTick')')) % turn scientific notation off
+
+get(gca, 'YTickLabel');
+new_yticks = {'0'; ''; '100,000'; ''; '200,000'; ''; '300,000'; ''; '400,000'};
+set(gca, 'yticklabel', new_yticks); 
+
+
+% Plot: mean of matches truncated above 99th percentile
+% title('D. Mean matches per patent below 99th percentile', 'FontSize', 14)
+
+
+% General settings
+set(gcf, 'Color', 'w');
+
+
+
+
+%% Change position and size
+set(gcf, 'Position', [100 200 700 400]) % in vector: left bottom width height
+set(figureHandle, 'Units', 'Inches');
+pos = get(figureHandle, 'Position');
+set(figureHandle, 'PaperPositionMode', 'Auto', 'PaperUnits', ...
+    'Inches', 'PaperSize', [pos(3), pos(4)])
+
+
+%% Add text
+% annotation('textbox', [0.21 0.14 0.1 0.1], 'String', '0.93', ...
+%         'FontSize', 12, 'HorizontalAlignment', 'left', ...
+%         'EdgeColor', 'none', 'Color', color2_pick); % [x y w h]
+% 
+% annotation('textbox', [0.41 0.335 0.1 0.1], 'String', '1.3', ...
+%         'FontSize', 12, 'HorizontalAlignment', 'left', ...
+%         'EdgeColor', 'none', 'Color', color2_pick); % [x y w h]
+% 
+% annotation('textbox', [0.67 0.05 0.1 0.1], 'String', '0.61', ...
+%         'FontSize', 12, 'HorizontalAlignment', 'left', ...
+%         'EdgeColor', 'none', 'Color', color2_pick); % [x y w h]
+% 
+% annotation('textbox', [0.85 0.175 0.1 0.1], 'String', '0.86', ...
+%         'FontSize', 12, 'HorizontalAlignment', 'left', ...
+%         'EdgeColor', 'none', 'Color', color2_pick); % [x y w h]
+
+
+%% Export to pdf
+print_pdf_name = horzcat('barchart_identified_patents_', num2str(year_start), '-',  num2str(year_end),'.pdf');
+print(figureHandle, print_pdf_name, '-dpdf', '-r0')
+
