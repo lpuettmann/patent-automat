f665efd68a379f853dd4982f3fd6150ccf6ba8f6
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-02-24 01:45:59 +0100

    ..

--- make_figures/plot_summary_statistics_overtime.m
+++ make_figures/plot_summary_statistics_overtime.m
@@ -29,105 +29,54 @@ plot_time = year_start:year_end;
 color1_pick = [0.7900, 0.3800, 0.500];
 color2_pick = [0.000,0.639,0.561];
 color3_pick = [0.890,0.412,0.525];
+color4_pick = [0.890,0.412,0.525];
+
 
 my_gray = [0.6706, 0.6706, 0.6706];
-
-% Give both subplots on mean matches the same scale
-mean_y_axis_limit = [0.5, 2];
 
 
 figureHandle = figure;
 
-
 % Plot total number of identified patents per year
-subplot(2, 2, 1)
-pick_plot_series = patent_match_summary.nr_patents_yr;
-plot(plot_time, pick_plot_series, 'Color', color1_pick, 'LineWidth', 1.4, ...
-    'Marker', 'o', 'MarkerSize', 3, 'MarkerFaceColor', color1_pick)
+subplot(2, 1, 1)
+bar(plot_time, patent_match_summary.nr_patents_yr, 'FaceColor', ...
+    color4_pick, 'EdgeColor', 'k')
+hold on
+bar(plot_time, patent_match_summary.nr_distinct_patents_hits, ...
+    'FaceColor', color1_pick, 'EdgeColor', 'k')
+hold off
+
 set(gca,'FontSize',12) % change default font size of axis labels
-title('A. Number of identified US patents per year', 'FontSize', 14)
+title('Number of identified US patents per year', 'FontSize', 14)
 box off
-xlim([year_start year_end]);
-ylim([0 400000]);
 set(gca,'TickDir','out'); 
+ylim([0 400000])
 set(gca, 'YTickLabel', num2str(get(gca, 'YTick')')) % turn scientific notation off
 
 get(gca, 'YTickLabel');
 new_yticks = {'0'; ''; '100,000'; ''; '200,000'; ''; '300,000'; ''; '400,000'};
 set(gca, 'yticklabel', new_yticks); 
 
-%gridxy(get(gca,'xtick'), get(gca,'ytick'), 'color', my_gray, 'linewidth', 1) % make grey grid lines
-
-
-% Plot: distinct patent matches
-subplot(2, 2, 2)
-pick_plot_series = patent_match_summary.nr_distinct_patents_hits;
-plot(plot_time, pick_plot_series, 'Color', color1_pick, 'LineWidth', 1.4, ...
-    'Marker', 'o', 'MarkerSize', 3, 'MarkerFaceColor', color1_pick)
-set(gca,'FontSize',12) % change default font size of axis labels
-title('B. Number of distinct patents that contain keyword', 'FontSize', 14)
-box off
-xlim([year_start year_end]);
-set(gca, 'YTickLabel', num2str(get(gca, 'YTick')')); % turn scientific notation off
-get(gca, 'YTickLabel');
-ylim([0 110000]);
-new_yticks = {'0'; '20,000'; '40,000'; '60,000'; '80,000'; '100,000'};
-set(gca, 'yticklabel', new_yticks); 
 
 
 % Plot: mean number of matches per year
-subplot(2, 2, 3)
+subplot(2, 1, 2)
 pick_plot_series = patent_match_summary.mean_patents_yr;
 plot(plot_time, pick_plot_series, 'Color', color2_pick, 'LineWidth', 1.4, ...
-    'Marker', 'o', 'MarkerSize', 3, 'MarkerFaceColor', color2_pick)
+    'Marker', 'o', 'MarkerSize', 4.5, 'MarkerFaceColor', color2_pick)
+hold on
+pick_plot_series = patent_match_summary.trunc_mean;
+plot(plot_time, pick_plot_series, 'Color', color2_pick, 'LineWidth', 1.4, ...
+    'Marker', 'o', 'MarkerSize', 4.5, 'MarkerFaceColor', color2_pick)
+hold off
+set(gca,'TickDir','out'); 
 set(gca,'FontSize',12) % change default font size of axis labels
-title('C. Mean number of keyword matches per patent', 'FontSize', 14)
+title('Mean number of keyword matches per patent', 'FontSize', 14)
 box off
-ylim(mean_y_axis_limit)
-xlim([year_start year_end])
-%set(gca, 'YTickLabel', num2str(get(gca, 'YTick')')) % turn scientific notation off
 
 
 % Plot: mean of matches truncated above 99th percentile
-subplot(2, 2, 4)
-pick_plot_series = patent_match_summary.trunc_mean;
-plot(plot_time, pick_plot_series, 'Color', color2_pick, 'LineWidth', 1.4, ...
-    'Marker', 'o', 'MarkerSize', 3, 'MarkerFaceColor', color2_pick)
-set(gca,'FontSize',12) % change default font size of axis labels
-title('D. Mean matches per patent below 99th percentile', 'FontSize', 14)
-box off
-ylim(mean_y_axis_limit)
-xlim([year_start year_end])
-%set(gca, 'YTickLabel', num2str(get(gca, 'YTick')')) % turn scientific notation off
-
-
-
-
-
-% Plot: maximum number of matches per patent per year
-% subplot(3, 2, 3)
-% pick_plot_series = patent_match_summary.max_patents_yr;
-% plot(plot_time, pick_plot_series, 'Color', color1_pick, 'LineWidth', 1.4, ...
-%     'Marker', 'o', 'MarkerSize', 3, 'MarkerFaceColor', color1_pick)
-% set(gca,'FontSize',12) % change default font size of axis labels
-% title('Maximum number of keyword matches per patent per year', 'FontSize', 14)
-% box off
-% xlim([year_start year_end])
-%set(gca, 'YTickLabel', num2str(get(gca, 'YTick')')) % turn scientific notation off
-
-
-
-% Plot: 99 percentile
-% subplot(3, 2, 5)
-% pick_plot_series = patent_match_summary.perc_99;
-% plot(plot_time, pick_plot_series, 'Color', color1_pick, 'LineWidth', 1.4, ...
-%     'Marker', 'o', 'MarkerSize', 3, 'MarkerFaceColor', color1_pick)
-% set(gca,'FontSize',12) % change default font size of axis labels
-% title('Cutoff for the 99th percentile', 'FontSize', 14)
-% box off
-% ylim([0 max(pick_plot_series)+6])
-% xlim([year_start year_end])
-%set(gca, 'YTickLabel', num2str(get(gca, 'YTick')')) % turn scientific notation off
+% title('D. Mean matches per patent below 99th percentile', 'FontSize', 14)
 
 
 % General settings
@@ -137,7 +86,7 @@ set(gcf, 'Color', 'w');
 
 
 %% Change position and size
-set(gcf, 'Position', [100 200 1300 800]) % in vector: left bottom width height
+set(gcf, 'Position', [100 200 900 600]) % in vector: left bottom width height
 set(figureHandle, 'Units', 'Inches');
 pos = get(figureHandle, 'Position');
 set(figureHandle, 'PaperPositionMode', 'Auto', 'PaperUnits', ...
