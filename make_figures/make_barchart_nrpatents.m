5f53d7c6e2a74a76eba182495659ced1ec81baf5
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-04-17 08:20:46 +0200

    ..

--- make_figures/make_barchart_nrpatents.m
+++ make_figures/make_barchart_nrpatents.m
@@ -4,7 +4,7 @@ clc
 
 % Define parameters
 year_start = 1976;
-year_end = 2014;
+year_end = 2015;
 
 
 
@@ -108,25 +108,35 @@ FID = fopen(printname, 'w');
 fprintf(FID,'\\begin{table}\n');
 fprintf(FID,'\\begin{small}\n');
 fprintf(FID,'\\begin{threeparttable}\n');
-fprintf(FID,'\\caption{{\\normalsize Overview}}\n');
+fprintf(FID,'\\caption{{\\normalsize Share of automation patents}}\n');
 fprintf(FID,'\\label{tab:word_distribution}\n');
 fprintf(FID,'\\begin{tabular}{lrrr}\n');
 fprintf(FID,'\\toprule \\addlinespace[0.5em]\n');
-fprintf(FID,' & \\textbf{Automation patents} & \\textbf{Total patents} & \\textbf{Share (\\%%)}\\tabularnewline[0.1cm]\n');
+fprintf(FID,' & \\textbf{Automation patents$^1$} & \\textbf{Total patents} & \\textbf{Share (\\%%)}\\tabularnewline[0.1cm]\n');
 fprintf(FID,'\\midrule \\addlinespace[0.5em]\n');
 
 for t=1:length(patent_match_summary.nr_patents_yr)
     ix_year = year_start + t - 1;
-    fprintf(FID,'%d & %d & %d & %3.1f \\tabularnewline[0.1cm]\n', ix_year, patent_match_summary.nr_distinct_patents_hits(t), patent_match_summary.nr_patents_yr(t), patent_match_summary.nr_distinct_patents_hits(t)/patent_match_summary.nr_patents_yr(t)*100);
+    
+    if ix_year == 2015
+        fprintf(FID,'2015$^2$ & %d & %d & %3.1f \\tabularnewline[0.1cm]\n', patent_match_summary.nr_distinct_patents_hits(t), patent_match_summary.nr_patents_yr(t), patent_match_summary.nr_distinct_patents_hits(t)/patent_match_summary.nr_patents_yr(t)*100);
+    else
+        fprintf(FID,'%d & %d & %d & %3.1f \\tabularnewline[0.1cm]\n', ix_year, patent_match_summary.nr_distinct_patents_hits(t), patent_match_summary.nr_patents_yr(t), patent_match_summary.nr_distinct_patents_hits(t)/patent_match_summary.nr_patents_yr(t)*100);
+    end
 end
 
-fprintf(FID,' & \\textbf{%d} & \\textbf{%d} & \\textbf{%3.1f} \\tabularnewline[0.1cm]\n', sum(patent_match_summary.nr_distinct_patents_hits), sum(patent_match_summary.nr_patents_yr), sum(patent_match_summary.nr_distinct_patents_hits)/sum(patent_match_summary.nr_patents_yr)*100);
+fprintf(FID,'\\textbf{Total} & \\textbf{%d} & \\textbf{%d} & \\textbf{%3.1f} \\tabularnewline[0.1cm]\n', sum(patent_match_summary.nr_distinct_patents_hits), sum(patent_match_summary.nr_patents_yr), sum(patent_match_summary.nr_distinct_patents_hits)/sum(patent_match_summary.nr_patents_yr)*100);
 
 fprintf(FID,'\\bottomrule\n');
 fprintf(FID,'\\end{tabular}\n');
 fprintf(FID,'\\begin{tablenotes}\n');
 fprintf(FID,'\\small\n');
-fprintf(FID,'\\item \\textit{Note:} Automation patents are defined as patents with at least one keyword match; both categories exclude named patents such as design patents.\n');
+fprintf(FID,'\\item $^1$: automation patents are defined as patents with at least one keyword match; both categories exclude named patents such as design patents.\n');
+
+if year_end == 2015
+    fprintf(FID,'\\item $^2$: up to 23.03.2015.\n');
+end
+
 fprintf(FID,'\\item \\textit{Source:} USPTO, Google and own calculations.\n');
 fprintf(FID,'\\end{tablenotes}\n');
 fprintf(FID,'\\end{threeparttable}\n');
