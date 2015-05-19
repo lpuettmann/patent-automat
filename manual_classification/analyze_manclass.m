3aac6387311c5abb4438945bd2449b194c3a3616
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-05-19 09:22:48 +0200

    Work on manual coding

--- manual_classification/analyze_manclass.m
+++ manual_classification/analyze_manclass.m
@@ -5,23 +5,17 @@ clc
 
 
 % Load excel file
-[manclass_data, txt, ~] = xlsread('manclass_FULL.xlsx');
-% [manclass_data, txt, ~] = xlsread('new_manclass_FULL.xlsx');
-
-
-
+[manclass_data, txt, ~] = xlsread('manclass_consolidated.xlsx');
 
 
 % Figure out how many patents have been classified yet
 indic_automat = manclass_data(:, 3);
 
-% if any((indic_automat ~= 0) | (indic_automat ~= 1) | (indic_automat ~= 1))
-%     warning('There should be only 0 and 1 and NaN here.')
-%     
-%     disp('Replace 99 with 0.')
-%     pos99 = find(indic_automat == 99);
-%     indic_automat(pos99) = 0;
-% end
+
+if any(not((indic_automat == 1) | (indic_automat == 0)))
+    warning('There should be only 0 and 1 here.')
+end
+
 
 last_codpt = find(isnan(indic_automat), 1) - 1;
 
@@ -43,9 +37,6 @@ nr_codpt = length(indic_automat);
 sum_automat = sum(indic_automat);
 share_automat = sum_automat / nr_codpt;
 
-fprintf('Number patents manually coded: %d, of those %d (%3.2f) automation patents.\n', ...
-    nr_codpt, sum_automat, share_automat)
-
 
 % Compare manual classification with automated keyword search
 nr_keyword_find = manclass_data(:, 7); % extract number of keyword matches
@@ -60,17 +51,31 @@ agree_rate = sum(indic_automat == pat_1match) / nr_codpt;
 
 complete_class = union(pos_pat_1match, pos_manclass_automat);
 overlap_class = intersect(pos_pat_1match, pos_manclass_automat);
-differ_class = setdiff(complete_class, ...
-    overlap_class);
+differ_class = setdiff(complete_class, overlap_class);
+
+manual1_automatic0 = setdiff(pos_manclass_automat, pos_pat_1match);
+automatic1_manual0 = setdiff(pos_pat_1match, pos_manclass_automat);
+
 
 fprintf('___________________________________________________________\n');
 fprintf('<strong>Comparison of manual and automatical patent classification</strong>\n');
 fprintf('  \n')
 
+fprintf('Number patents manually coded: %d.\n', ...
+    nr_codpt)
+fprintf('Of those manually classified as automation patents: %d (%3.2f) .\n', ...
+   sum_automat, share_automat)
+fprintf('Of those automatically classified as automation patents: %d (%3.2f) .\n', ...
+   sum(pat_1match), sum(pat_1match) / nr_codpt)
+
 fprintf('Overall agreement: %d/%d (%3.2f).\n', ...
     sum(indic_automat == pat_1match), nr_codpt, agree_rate)
 fprintf('Same classifications: %d/%d (%3.2f).\n', length(overlap_class), ...
     length(complete_class), length(overlap_class) / length(complete_class))
+fprintf('manual1_automatic0: %d.\n', length(manual1_automatic0))
+fprintf('automatic1_manual0: %d.\n', length(automatic1_manual0))
+
+
 fprintf('Differing classifications: %d/%d (%3.2f).\n', length(differ_class), ...
     length(complete_class), length(differ_class) / length(complete_class))
 
