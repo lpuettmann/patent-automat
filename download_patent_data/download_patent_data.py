e8b257084d06a448d533bbc5822fe1b5ba595758
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-05-20 17:01:26 +0200

    Minor change

--- download_patent_data/download_patent_data.py
+++ download_patent_data/download_patent_data.py
@@ -1,18 +1,44 @@
-# http://yizeng.me/2014/05/23/download-pdf-files-automatically-in-firefox-using-selenium-webdriver/
 
 from selenium import webdriver 
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
+
+# Define parameters
+# ----------------------------------------------------------------
+savepath = 'D:\\' # set the desired path here for the files
+#savepath = 'D:\\2015' # set the desired path here for the files
+fname = ["ipg150106.zip", "ipg150113.zip"]
+
+
+# Download the files from Google Patents
+# ----------------------------------------------------------------
 profile = FirefoxProfile ()
 profile.set_preference("browser.download.panel.shown", False) 
 
 profile.set_preference("browser.download.folderList", 2) # 2 means specify custom location
 profile.set_preference("browser.download.manager.showWhenStarting", False)
-profile.set_preference("browser.download.dir", 'D:\\') # choose folder to download to
+profile.set_preference("browser.download.dir", savepath) # choose folder to download to
 profile.set_preference("browser.helperApps.neverAsk.saveToDisk",'application/octet-stream')
 
 driver = webdriver.Firefox(firefox_profile=profile)
 driver.get('https://www.google.com/googlebooks/uspto-patents-grants-text.html#2015')
 
-filename = driver.find_element_by_xpath('//a[contains(text(),"ipg150106.zip")]') # use loop to list all zip files
+print('Enter loop:')
+
+#for f in fname:
+	#filename = driver.find_element_by_xpath('//a[contains(text(), f)]') # use loop to list all zip files
+	#filename.click()
+	#print('Finished loop for: {}.'.format(f))
+
+# print(filename)
+
+# print('Try it out again')
+# filename.click()
+
+filename = driver.find_element_by_xpath('//a[contains(text(), "ipg150106.zip")]') # use loop to list all zip files
 filename.click()
+
+#filename = driver.find_element_by_xpath('//a[contains(text(), "ipg150113.zip")]') # use loop to list all zip files
+#filename.click()
+
+print('End.')
