06131d976446f0f64192100b44a6889b630e033f
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-06-19 14:32:43 +0200

    Compare algorithms in contingency table

--- manual_classification/analyze_manclass.m
+++ manual_classification/analyze_manclass.m
@@ -4,146 +4,173 @@ clc
 
 addpath('../functions')
 
-%% Parameters
+% Parameters
 year_start = 1976;
 year_end = 2015;
 
 
-%% Load previously extracted data
+% Load previously extracted data
 load('manclass_data.mat')
 
 
-%% Delete those patents with technology numbers some industries
+% Iterate through classification algorithm set-ups
+% ========================================================================
 
-delete_technr = [430, % Radiation Imagery Chemistry
-                 431, % Combustion
-                 435, % Chemistry: Molecular Biology and Microbiology
-                 514, % Drug, Bio-affecting and Body Treating Compositions
-                 530, % Chemistry: Natural Resins or Derivatives; Peptides or Proteins; ...
-                % 600, % Surgery
-                % 606, % Surgery
-                 800]; % Multicellar Living Organisms
-             
-technr = manclass_data(:, 8);
-delete_pat_pos = find(ismember(technr, delete_technr));
+nr_alg = 2;
 
-del_pat = manclass_data(delete_pat_pos, :);
-del_pat_manual1 = find(del_pat(:, 3));
-del_pat_automatic1 = find(del_pat(:, 7)>0);
-del_pat_manual1_automatic1 = intersect(del_pat_manual1, del_pat_automatic1);
-del_pat_manual1_automatic0 = setdiff(del_pat_manual1, del_pat_automatic1);
-del_pat_automatic1_manual0 = setdiff(del_pat_automatic1, del_pat_manual1);
+for i=1:nr_alg
 
-fprintf('___________________________________________________________\n');
-fprintf('<strong>Discarded technologies</strong>\n');
-fprintf(' \n')
-fprintf('Delete %d/%d (%3.2f) patents.\n', length(delete_pat_pos), ...
-    size(manclass_data, 1), length(delete_pat_pos)/size(manclass_data, 1))
-fprintf('Of those: \n')
-fprintf('\t %d = manual: 1, automatic: 1.\n', length(del_pat_manual1_automatic1))
-fprintf('\t %d = manual: 1, automatic: 0.\n', length(del_pat_manual1_automatic0))
-fprintf('\t %d = automatic: 1, manual: 0.\n', length(del_pat_automatic1_manual0))
-fprintf(' ')
+    if i==2
+        % Delete those patents with technology numbers some industries
+        delete_technr = [430, % Radiation Imagery Chemistry
+                         431, % Combustion
+                         435, % Chemistry: Molecular Biology and Microbiology
+                         514, % Drug, Bio-affecting and Body Treating Compositions
+                         530, % Chemistry: Natural Resins or Derivatives; Peptides or Proteins; ...
+                        % 600, % Surgery
+                        % 606, % Surgery
+                         800]; % Multicellar Living Organisms
 
-manclass_data(delete_pat_pos, :) = [];
+        technr = manclass_data(:, 8);
+        delete_pat_pos = find(ismember(technr, delete_technr));
 
+        del_pat = manclass_data(delete_pat_pos, :);
+        del_pat_manual1 = find(del_pat(:, 3));
+        del_pat_automatic1 = find(del_pat(:, 7)>0);
+        del_pat_manual1_automatic1 = intersect(del_pat_manual1, del_pat_automatic1);
+        del_pat_manual1_automatic0 = setdiff(del_pat_manual1, del_pat_automatic1);
+        del_pat_automatic1_manual0 = setdiff(del_pat_automatic1, del_pat_manual1);
 
-%% Transform variables
+        fprintf('___________________________________________________________\n');
+        fprintf('<strong>Discarded technologies</strong>\n');
+        fprintf(' \n')
+        fprintf('Delete %d/%d (%3.2f) patents.\n', length(delete_pat_pos), ...
+            size(manclass_data, 1), length(delete_pat_pos)/size(manclass_data, 1))
+        fprintf('Of those: \n')
+        fprintf('\t %d = manual: 1, automatic: 1.\n', length(del_pat_manual1_automatic1))
+        fprintf('\t %d = manual: 1, automatic: 0.\n', length(del_pat_manual1_automatic0))
+        fprintf('\t %d = automatic: 1, manual: 0.\n', length(del_pat_automatic1_manual0))
+        fprintf(' ')
 
-% Figure out how many patents have been classified yet
-indic_automat = manclass_data(:, 3);
+        manclass_data(delete_pat_pos, :) = [];
+    end
 
-classifstat.nr_codpt = length(indic_automat);
-classifstat.sum_automat = sum(indic_automat);
-share_automat = classifstat.sum_automat / classifstat.nr_codpt;
+    % Transform variables
 
+    % Figure out how many patents have been classified yet
+    indic_automat = manclass_data(:, 3);
 
-%% Compare manual classification with automated keyword search
-nr_keyword_find = manclass_data(:, 7); % extract number of keyword matches
-% Find patents with at least 1 match
-classifstat.pat_1match = (nr_keyword_find > 0); % the plus converts logical to double 
-
-pos_manclass_automat = find(indic_automat);
-pos_pat_1match = find(classifstat.pat_1match);
+    classifstat.nr_codpt = length(indic_automat);
+    classifstat.sum_automat = sum(indic_automat);
+    share_automat = classifstat.sum_automat / classifstat.nr_codpt;
 
 
-%% Calculate area under curve (AUROC)
-% -------------------------------------------------------------------
-classifstat.auc = calculate_auc(indic_automat, classifstat.pat_1match);
+    % Compare manual classification with automated keyword search
+    nr_keyword_find = manclass_data(:, 7); % extract number of keyword matches
+    % Find patents with at least 1 match
+    classifstat.pat_1match = (nr_keyword_find > 0); % the plus converts logical to double 
+
+    pos_manclass_automat = find(indic_automat);
+    pos_pat_1match = find(classifstat.pat_1match);
 
 
-%% Calculate the overall agreement rate
-
-% For  definitions of evaluation measures (accuracy, precision, recall,
-% fmeasure...), see:
-%   Manning, Raghavan, Schütze "Introduction to Information Retrieval",
-%   first edition (2008), section "8. Evaluation in information retrieval"
-
-classifstat.accuracy = sum(indic_automat == classifstat.pat_1match) / classifstat.nr_codpt;
-complete_class = union(pos_pat_1match, pos_manclass_automat);
-classifstat.overlap_class = intersect(pos_pat_1match, pos_manclass_automat);
-differ_class = setdiff(complete_class, classifstat.overlap_class);
-
-classifstat.precision = length(classifstat.overlap_class) / sum(classifstat.pat_1match);
-classifstat.recall = length(classifstat.overlap_class) / classifstat.sum_automat;
-
-classifstat.evalalpha = 1/2; % alpha
-classifstat.evalbeta_squared = (1 - classifstat.evalalpha) / classifstat.evalalpha; % beta squared
-
-classifstat.fmeasure = ((classifstat.evalbeta_squared + 1) * classifstat.precision * ...
-    classifstat.recall) / (classifstat.evalbeta_squared * classifstat.precision + ...
-    classifstat.recall);
+    % Calculate area under curve (AUROC)
+    % -------------------------------------------------------------------
+    classifstat.auc = calculate_auc(indic_automat, classifstat.pat_1match);
 
 
-classifstat.manual1_automatic0 = setdiff(pos_manclass_automat, pos_pat_1match);
-classifstat.automatic1_manual0 = setdiff(pos_pat_1match, pos_manclass_automat);
+    % Calculate the overall agreement rate
+
+    % For  definitions of evaluation measures (accuracy, precision, recall,
+    % fmeasure...), see:
+    %   Manning, Raghavan, Schütze "Introduction to Information Retrieval",
+    %   first edition (2008), section "8. Evaluation in information retrieval"
+
+    classifstat.accuracy = sum(indic_automat == classifstat.pat_1match) / classifstat.nr_codpt;
+    complete_class = union(pos_pat_1match, pos_manclass_automat);
+    classifstat.overlap_class = intersect(pos_pat_1match, pos_manclass_automat);
+    differ_class = setdiff(complete_class, classifstat.overlap_class);
+
+    classifstat.precision = length(classifstat.overlap_class) / sum(classifstat.pat_1match);
+    classifstat.recall = length(classifstat.overlap_class) / classifstat.sum_automat;
+
+    classifstat.evalalpha = 1/2; % alpha
+    classifstat.evalbeta_squared = (1 - classifstat.evalalpha) / classifstat.evalalpha; % beta squared
+
+    classifstat.fmeasure = ((classifstat.evalbeta_squared + 1) * classifstat.precision * ...
+        classifstat.recall) / (classifstat.evalbeta_squared * classifstat.precision + ...
+        classifstat.recall);
 
 
-classifstat.patents_automatic1_manual0 = manclass_data(...
-    classifstat.automatic1_manual0, :);
+    classifstat.manual1_automatic0 = setdiff(pos_manclass_automat, pos_pat_1match);
+    classifstat.automatic1_manual0 = setdiff(pos_pat_1match, pos_manclass_automat);
 
 
-%% Analyze those patents that were manually classified as automation patents
-matches_manclasspt = nr_keyword_find(pos_manclass_automat);
-matches_rest = nr_keyword_find(not(indic_automat));
+    classifstat.patents_automatic1_manual0 = manclass_data(...
+        classifstat.automatic1_manual0, :);
+
+
+    % Analyze those patents that were manually classified as automation patents
+    matches_manclasspt = nr_keyword_find(pos_manclass_automat);
+    matches_rest = nr_keyword_find(not(indic_automat));
 
 
 
-%% Errors over time
-% -------------------------------------------------------------------
+    % Errors over time
+    % -------------------------------------------------------------------
 
-% Extract
-for ix_year=year_start:year_end
-    t = ix_year - year_start + 1;
-    
-    yr_pos = find(ix_year == manclass_data(:, 2));
-    
-    yr_indic_automat = manclass_data(yr_pos, 3);
-    yr_pat_1match = manclass_data(yr_pos, end)>0;
-    
-    classifstat.number(t) = length(yr_pos);
-    classifstat.summanclass(t) = sum(yr_indic_automat);
-    classifstat.sumpat1match(t) = sum(yr_pat_1match);
-    classifstat.agreerate(t) = sum(yr_indic_automat==yr_pat_1match) / ...
-        length(yr_pos);
-    classifstat.manautom(t) = sum(yr_indic_automat) / length(yr_pos);
-    classifstat.compautom(t) = sum(yr_pat_1match) / length(yr_pos);
+    % Extract
+    for ix_year=year_start:year_end
+        t = ix_year - year_start + 1;
+
+        yr_pos = find(ix_year == manclass_data(:, 2));
+
+        yr_indic_automat = manclass_data(yr_pos, 3);
+        yr_pat_1match = manclass_data(yr_pos, end)>0;
+
+        classifstat.number(t) = length(yr_pos);
+        classifstat.summanclass(t) = sum(yr_indic_automat);
+        classifstat.sumpat1match(t) = sum(yr_pat_1match);
+        classifstat.agreerate(t) = sum(yr_indic_automat==yr_pat_1match) / ...
+            length(yr_pos);
+        classifstat.manautom(t) = sum(yr_indic_automat) / length(yr_pos);
+        classifstat.compautom(t) = sum(yr_pat_1match) / length(yr_pos);
+    end
+
+
+    % Plausibility checks
+    % -------------------------------------------------------------------
+    if abs(mean(classifstat.manautom) - share_automat) > 0.1 * mean(classifstat.manautom)
+        warning('Quite different.')
+    end
+
+    if abs(mean(classifstat.compautom) - (sum(classifstat.pat_1match) / classifstat.nr_codpt)) > 0.1 * mean(classifstat.compautom)
+        warning('Quite different.')
+    end
+
+
+    % Save to .mat file
+    % -------------------------------------------------------------------
+    save_name = 'classifstat.mat';
+    save(save_name, 'classifstat');    
+    fprintf('Saved: %s.\n', save_name)
+
+
+    % Put into format to be put in contingency table
+    % -------------------------------------------------------------------
+    conttab.val(:,:,i) = [classifstat.nr_codpt - classifstat.sum_automat - length(classifstat.automatic1_manual0), length(classifstat.automatic1_manual0), classifstat.nr_codpt - classifstat.sum_automat;
+        length(classifstat.manual1_automatic0), classifstat.sum_automat - length(classifstat.manual1_automatic0), classifstat.sum_automat;
+        classifstat.nr_codpt - sum(classifstat.pat_1match), sum(classifstat.pat_1match), classifstat.nr_codpt];
+
+    conttab.stats(:,:,i) = {[length(classifstat.overlap_class), classifstat.nr_codpt - classifstat.sum_automat - length(classifstat.automatic1_manual0), classifstat.nr_codpt, classifstat.accuracy];
+        [length(classifstat.overlap_class), sum(classifstat.pat_1match), classifstat.precision];
+        [length(classifstat.overlap_class),  classifstat.sum_automat, classifstat.recall];
+        [sqrt(classifstat.evalbeta_squared), classifstat.fmeasure];
+        classifstat.auc};
 end
 
-
-%% Plausibility checks
+% Save to .mat file
 % -------------------------------------------------------------------
-if abs(mean(classifstat.manautom) - share_automat) > 0.1 * mean(classifstat.manautom)
-    warning('Quite different.')
-end
-
-if abs(mean(classifstat.compautom) - (sum(classifstat.pat_1match) / classifstat.nr_codpt)) > 0.1 * mean(classifstat.compautom)
-    warning('Quite different.')
-end
-
-
-%% Save to .mat file
-% -------------------------------------------------------------------
-save_name = 'classifstat.mat';
-save(save_name, 'classifstat');    
+save_name = 'conttab.mat';
+save(save_name, 'conttab');    
 fprintf('Saved: %s.\n', save_name)
--- manual_classification/make_manclass_contingency_table.m
+++ manual_classification/make_manclass_contingency_table.m
@@ -6,7 +6,7 @@ clc
 
 
 %% Load stats on classifications
-load('classifstat.mat')
+load('conttab.mat')
 
 
 %% Print to .txt file in Latex format
@@ -19,15 +19,16 @@ fprintf(FID,'\\begin{small}\n');
 fprintf(FID,'\\begin{threeparttable}\n');
 fprintf(FID,'\\caption{{\\normalsize Contingency table}}\n');
 fprintf(FID,'\\label{table:contingency_classifications}\n');
-fprintf(FID,'\\begin{tabular}{ll|ll|l}\n');
+fprintf(FID,'\\begin{tabular}{ll|ll|ll|ll|ll}\n');
 
-fprintf(FID, '& \\multicolumn{4}{c}{Computerized} \\tabularnewline[0.1cm]\n');
-fprintf(FID, '& & No & Yes &   \\tabularnewline\n');
-fprintf(FID, '\\cline{2-5}\n');
-fprintf(FID, '\\parbox[t]{2mm}{\\multirow{2}{*}{\\rotatebox[origin=c]{90}{Manual}}} & No & %d & %d & %d \\tabularnewline\n', classifstat.nr_codpt - classifstat.sum_automat - length(classifstat.automatic1_manual0), length(classifstat.automatic1_manual0), classifstat.nr_codpt - classifstat.sum_automat);
-fprintf(FID, '& Yes & %d & %d & %d \\tabularnewline\n', length(classifstat.manual1_automatic0), classifstat.sum_automat - length(classifstat.manual1_automatic0), classifstat.sum_automat);
-fprintf(FID, '\\cline{2-5}\n');
-fprintf(FID, '&  & %d & %d & %d \\tabularnewline\n', classifstat.nr_codpt - sum(classifstat.pat_1match), sum(classifstat.pat_1match), classifstat.nr_codpt);
+fprintf(FID, '& & \\multicolumn{7}{c}{Computerized} \\tabularnewline[0.1cm]\n');
+fprintf(FID, '& & \\multicolumn{3}{c}{$A_1$} & & \\multicolumn{3}{c}{$A_2$} \\tabularnewline[0.1cm]\n');
+fprintf(FID, '& & No & Yes & & & No & Yes &  \\tabularnewline\n');
+fprintf(FID, '\\cline{2-5} \\cline{7-9}\n');
+fprintf(FID, '\\parbox[t]{2mm}{\\multirow{2}{*}{\\rotatebox[origin=c]{90}{Manual}}} & No & %d & %d & %d & & %d & %d & %d \\tabularnewline\n', conttab.val(1,:,1), conttab.val(1,:,2));
+fprintf(FID, '& Yes & %d & %d & %d & & %d & %d & %d \\tabularnewline\n', conttab.val(2,:,1), conttab.val(2,:,2));
+fprintf(FID, '\\cline{2-5} \\cline{7-9}\n');
+fprintf(FID, '&  & %d & %d & %d & & %d & %d & %d \\tabularnewline\n', conttab.val(3,:,1), conttab.val(3,:,2));
 
 fprintf(FID,'\\end{tabular}\n');
 fprintf(FID,'\\end{threeparttable}\n');
@@ -37,15 +38,13 @@ fprintf(FID,'\\floatfoot{');
 fprintf(FID,'\\begin{minipage}{0.3\\textwidth}');
 fprintf(FID,' \\textit{Statistics:}\\\\\n');
 fprintf(FID,'Accuracy = $\\frac{%d + %d}{%d}$ = %3.2f\\\\\n', ...
-    length(classifstat.overlap_class), classifstat.nr_codpt - classifstat.sum_automat - ...
-    length(classifstat.automatic1_manual0), classifstat.nr_codpt, ...
-    classifstat.accuracy);
-fprintf(FID,'Precision = $\\frac{%d}{%d}$ = %3.2f\\\\\n', ...
-    length(classifstat.overlap_class), sum(classifstat.pat_1match), classifstat.precision);
-fprintf(FID,'Recall = $\\frac{%d}{%d}$ = %3.2f\\\\\n', ...
-    length(classifstat.overlap_class),  classifstat.sum_automat, classifstat.recall);
-fprintf(FID,'$F_{\\beta = %d}$ = %3.2f\\\\\n', sqrt(classifstat.evalbeta_squared), classifstat.fmeasure);
-fprintf(FID,'AUC = %3.3f\n', classifstat.auc);
+    conttab.stats{1});
+fprintf(FID,'Precision = $\\frac{%d}{%d}$ = %3.2f\\\\\n', conttab.stats{2});
+    
+fprintf(FID,'Recall = $\\frac{%d}{%d}$ = %3.2f\\\\\n', conttab.stats{3});
+    
+fprintf(FID,'$F_{\\beta = %d}$ = %3.2f\\\\\n', conttab.stats{4});
+fprintf(FID,'AUC = %3.3f\n', conttab.stats{5});
 fprintf(FID,'\\end{minipage}}');
 
 fprintf(FID,'\\end{table}\n');
@@ -53,3 +52,7 @@ fclose(FID);
 
 fprintf('___________________________________________________________\n');
 fprintf('Saved: %s.\n', printname)
+
+
+run ../copy_selected_files
+warning('Automatically copy file over to project paper directory.')
