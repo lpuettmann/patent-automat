2f786030af2d880c35766a76d65d5087cd3da489
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-06-19 15:44:43 +0200

    Table is nice

--- manual_classification/make_manclass_contingency_table.m
+++ manual_classification/make_manclass_contingency_table.m
@@ -32,14 +32,14 @@ fprintf(FID, '\\parbox[t]{2mm}{\\multirow{2}{*}{\\rotatebox[origin=c]{90}{Manual
 fprintf(FID, '& Yes & \\multicolumn{1}{|l}{%d} & \\multicolumn{1}{l|}{%d} & %d & & \\multicolumn{1}{|l}{%d} & \\multicolumn{1}{l|}{%d} & %d \\tabularnewline\n', conttab.val(2,:,1), conttab.val(2,:,2));
 fprintf(FID, '\\cline{2-5} \\cline{7-9}\n');
 fprintf(FID, '&  & \\multicolumn{1}{|l}{%d} & \\multicolumn{1}{l|}{%d} & %d & & \\multicolumn{1}{|l}{%d} & \\multicolumn{1}{l|}{%d} & %d \\tabularnewline\n', conttab.val(3,:,1), conttab.val(3,:,2));
-fprintf(FID, '&  &  & &  & &  &  &  \\tabularnewline[0.3cm]\n');
+fprintf(FID, '&  &  & &  & &  &  &  \\tabularnewline[-0.1cm]\n');
 fprintf(FID, '\\cline{3-5} \\cline{7-9}\n');
-fprintf(FID, '& Accuracy & \\multicolumn{3}{r}{$\\frac{%d + %d}{%d} = %3.2f$} & & \\multicolumn{3}{r}{$\\frac{%d + %d}{%d} = %3.2f$}  \\tabularnewline\n', conttab.stats{1,:,1}, conttab.stats{1,:,2});
-fprintf(FID, '& Precision & \\multicolumn{3}{r}{$\\frac{%d}{%d} = %3.2f$} & & \\multicolumn{3}{r}{$\\frac{%d}{%d} = %3.2f$} \\tabularnewline\n', conttab.stats{2,:,1}, conttab.stats{2,:,2});
-fprintf(FID, '& Recall & \\multicolumn{3}{r}{$\\frac{%d}{%d} = %3.2f$} & & \\multicolumn{3}{r}{$\\frac{%d}{%d} = %3.2f$} \\tabularnewline\n', conttab.stats{3,:,1}, conttab.stats{3,:,2});
+fprintf(FID, '\\rule{0pt}{12pt}& Accuracy & \\multicolumn{3}{r}{$\\frac{%d + %d}{%d} = %3.2f$} & & \\multicolumn{3}{r}{$\\frac{%d + %d}{%d} = %3.2f$}  \\tabularnewline\n', conttab.stats{1,:,1}, conttab.stats{1,:,2});
+fprintf(FID, '\\rule{0pt}{12pt}& Precision & \\multicolumn{3}{r}{$\\frac{%d}{%d} = %3.2f$} & & \\multicolumn{3}{r}{$\\frac{%d}{%d} = %3.2f$} \\tabularnewline\n', conttab.stats{2,:,1}, conttab.stats{2,:,2});
+fprintf(FID, '\\rule{0pt}{12pt}& Recall & \\multicolumn{3}{r}{$\\frac{%d}{%d} = %3.2f$} & & \\multicolumn{3}{r}{$\\frac{%d}{%d} = %3.2f$} \\tabularnewline\n', conttab.stats{3,:,1}, conttab.stats{3,:,2});
 % Only print the beta once
-fprintf(FID, '& $F_{\\beta = %d}$ & \\multicolumn{3}{r}{%3.2f} & & \\multicolumn{3}{r}{%3.2f} \\tabularnewline\n', conttab.stats{4,:,1}(1), conttab.stats{4,:,1}(2), conttab.stats{4,:,2}(2));
-fprintf(FID, '& AUC & \\multicolumn{3}{r}{%3.2f} & & \\multicolumn{3}{r}{%3.2f} \\tabularnewline\n', conttab.stats{5,:,1}, conttab.stats{5,:,2});
+fprintf(FID, '\\rule{0pt}{12pt}& $F_{\\beta = %d}$ & \\multicolumn{3}{r}{%3.2f} & & \\multicolumn{3}{r}{%3.2f} \\tabularnewline\n', conttab.stats{4,:,1}(1), conttab.stats{4,:,1}(2), conttab.stats{4,:,2}(2));
+fprintf(FID, '\\rule{0pt}{12pt}& AUC & \\multicolumn{3}{r}{%3.2f} & & \\multicolumn{3}{r}{%3.2f} \\tabularnewline\n', conttab.stats{5,:,1}, conttab.stats{5,:,2});
 fprintf(FID, '\\bottomrule');
 fprintf(FID,'\\end{tabular}\n');
 fprintf(FID,'\\end{threeparttable}\n');
