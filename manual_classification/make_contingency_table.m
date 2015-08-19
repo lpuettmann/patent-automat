adf1a56067f27063957a87370480a7cba2ef9672
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-08-19 10:29:39 +0200

    Add superscript 1 to fmeasure in table + change name from handbook to guidelines

--- manual_classification/make_contingency_table.m
+++ manual_classification/make_contingency_table.m
@@ -26,7 +26,7 @@ fprintf(FID, '\\rule{0pt}{12pt} & Accuracy & \\multicolumn{3}{r}{$\\frac{%d + %d
 fprintf(FID, '\\rule{0pt}{12pt}& Precision & \\multicolumn{3}{r}{$\\frac{%d}{%d} = %3.2f$} & \\tabularnewline\n', classifstat.true_positive, classifstat.false_positive + classifstat.true_positive, classifstat.precision);
 fprintf(FID, '\\rule{0pt}{12pt}& Recall & \\multicolumn{3}{r}{$\\frac{%d}{%d} = %3.2f$} &  \\tabularnewline\n', classifstat.true_positive, classifstat.false_negative + classifstat.true_positive, classifstat.recall);
 % Only print the beta once
-fprintf(FID, '\\rule{0pt}{12pt}& $F_1$ & \\multicolumn{3}{r}{%3.2f} & \\tabularnewline\n', classifstat.fmeasure);
+fprintf(FID, '\\rule{0pt}{12pt}& $F_1^1$ & \\multicolumn{3}{r}{%3.2f} & \\tabularnewline\n', classifstat.fmeasure);
 fprintf(FID, '\\rule{0pt}{12pt}& AUC$^2$ & \\multicolumn{3}{r}{%3.2f} & \\tabularnewline\n', classifstat.auc);
 fprintf(FID, '\\rule{0pt}{12pt}& MCC$^3$ & \\multicolumn{3}{r}{%3.2f} & \\tabularnewline\n', classifstat.matthewscorrcoeff);
 
@@ -41,6 +41,7 @@ fprintf(FID,'\\item $^2$: Area under (the receiver operating) curve.\n');
 fprintf(FID,'\\item $^3$: Matthew''s correlation coefficient.\n');
 fprintf(FID,'\\item\\textit{Source:} USPTO, Google and own calculations.\n');
 fprintf(FID,'\\end{tablenotes}\n');
+% fprintf(FID, '\\multicolumn{6}{l}{\\textit{Source:} USPTO, Google and own calculations.} \\tabularnewline\n');
 fprintf(FID,'\\end{threeparttable}\n');
 fprintf(FID,'\\end{small}\n');
 fprintf(FID,'\\end{table}\n');
