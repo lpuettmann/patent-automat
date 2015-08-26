26e258d0bafddea8079001b68a22464d7d13a354
lpuettmann <lukas.puettmann@uni-bonn.de>
2015-08-26 17:48:54 +0200

    Improve bilateral plot

--- industry_analysis/make_bivariate_plot.m
+++ industry_analysis/make_bivariate_plot.m
@@ -20,20 +20,58 @@ laborm_mean = get_interv_means(interv_length, laborm_series_sorted);
 manuf_employment = sum(laborm_mean, 2);
 
 
+% Get share of automation patents per industry
+automat_ishare = nan(size(laborm_series));
+for ix_industry = 1:size(laborm_series, 2)
+    for i=1:nr_fullyears       
+        automat_ishare(i, ix_industry) = industry_sumstats(ix_industry, 2, i) ./ ...
+            industry_sumstats(ix_industry, 1, i);
+    end
+end
+
+automat_ishare_mean = get_interv_means(interv_length, automat_ishare);
+
+
+% Sort share of automation patents
+automat_ishare_mean_sorted = automat_ishare_mean(:, ix_sort);
+
+
 % Set font
 set(0, 'DefaultTextFontName', 'Palatino')
 set(0, 'DefaultAxesFontName', 'Palatino')
 
-colors = [141,211,199;
-        255,255,179;
-        190,186,218;
-        251,128,114;
-        128,177,211;
-        253,180,98;
-        179,222,105;
-        252,205,229;
-        217,217,217;
-        188,128,189] ./ 255;
+% colors = [141,211,199;
+%         255,255,179;
+%         190,186,218;
+%         251,128,114;
+%         128,177,211;
+%         253,180,98;
+%         179,222,105;
+%         252,205,229;
+%         217,217,217;
+%         188,128,189] ./ 255;
+    
+colors = [%81,87,74;
+        68,124,105;
+%         116,196,147;
+%         142,140,109;
+%         228,191,128;
+        233,215,142;
+%         226,151,93;
+%         241,150,112;
+%         225,101,82;
+        201,74,83;
+%         190,81,104;
+        163,73,116;
+%         153,55,103;
+        101,56,125;
+%         78,36,114;
+        145,99,182;
+%         226,121,16;
+        224,89,139;
+        124,159,176;
+        86,152,196;
+        154,191,136] ./ 255;
 
 nr_colors = size(colors, 1);
 
@@ -54,18 +92,12 @@ ix_color = 1; % initialize color for first industry group
 for ix_industry = 1:size(laborm_series, 2)
     single_laborm_series = laborm_mean(:, ix_industry);
     
-    % Divide by total number of employees in manufacturing sector
+    % Calculate ratio 
     ishare_manuf = single_laborm_series ./ manuf_employment;
 
-
-    automat_ishare = nan(nr_fullyears, 1);
-    for i=1:nr_fullyears       
-        automat_ishare(i) = industry_sumstats(ix_industry, 2, i) ./ ...
-            industry_sumstats(ix_industry, 1, i);
-    end
+    % Extract series of shares of automation patents for specific industry
+    single_automat_ishare = automat_ishare_mean_sorted(:, ix_industry);
     
-    automat_ishare_mean = get_interv_means(interv_length, automat_ishare);
-
     % Determine which industry group the industry belongs to
     color1_pick = colors(sortList(ix_industry), :);
     
@@ -88,11 +120,11 @@ for ix_industry = 1:size(laborm_series, 2)
 %         marker_pick = 'd';
 %     end
         
-    plot(automat_ishare_mean(indic_dot:end), ishare_manuf(indic_dot:end), ...
+    plot(single_automat_ishare(indic_dot:end), ishare_manuf(indic_dot:end), ...
         'Color', color1_pick, 'Marker', marker_pick, ...
         'MarkerFaceColor', color1_pick, 'MarkerSize', marker_size, 'LineWidth', line_width)
     hold on
-    hStartDot = plot(automat_ishare_mean(1:indic_dot), ishare_manuf(1:indic_dot), 'Color', color1_pick, ...
+    hStartDot = plot(single_automat_ishare(1:indic_dot), ishare_manuf(1:indic_dot), 'Color', color1_pick, ...
         'Marker', marker_pick, 'MarkerSize', marker_size, 'LineWidth', line_width);
     hold on
     
@@ -115,12 +147,12 @@ set(gca, 'TickDir', 'out')
 
 legend1 = legend(industry_list_sorted, 'Fontsize', 9, 'Location', 'NorthEast');
 legend boxoff
-set(legend1,...
-    'Position', [0.612598393330502 0.229465274481886 0.387402933563417 0.636627906976745])
+% set(legend1,...
+%     'Position', [0.612598393330502 0.229465274481886 0.387402933563417 0.636627906976745])
 
 
 % Change position and size
-set(gcf, 'Position', [100 200 1500 800]) % in vector: left bottom width height
+set(gcf, 'Position', [100 200 1200 800]) % in vector: left bottom width height
 set(figureHandle, 'Units', 'Inches');
 pos = get(figureHandle, 'Position');
 set(figureHandle, 'PaperPositionMode', 'Auto', 'PaperUnits', ...
