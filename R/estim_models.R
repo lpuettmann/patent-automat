22b79f07568d461e5fafb977855e5c689545a231
lpuettmann <lukas.puettmann@uni-bonn.de>
2017-03-13 11:42:28 +0100

    include patent length

--- R/estim_models.R
+++ R/estim_models.R
@@ -23,13 +23,15 @@ cat(paste("done. [", round(proc.time()[3] - tic, digits = 1), "s]\n", sep = ""))
 # Drop all patents with flagged technology numbers (chemical + pharma)
 pdata <- pdata %>% filter(indic_exclclassnr == 0)
 
-# Outcome variable: manual classification of whether a patent is an 
-# automation patent
+pdata$logPatLen <- log(pdata$line_end - pdata$line_start)
+
+# Outcome variable: patent is automation patent
 fullData <- cbind(manAutomat = pdata$manAutomat,
                   ipc_ocat = pdata$ipc_ocat,
-                    select(pdata, starts_with("t_")),
-                    select(pdata, starts_with("a_")),
-                    select(pdata, starts_with("b_")))
+                  logPatLen = pdata$logPatLen,
+                  log(1 + select(pdata, starts_with("t_"))),
+                  log(1 + select(pdata, starts_with("a_"))),
+                  log(1 + select(pdata, starts_with("b_"))))
 
 smp_size <- floor(0.75 * nrow(fullData))  # pick training set size
 set.seed(123)                             # make partition reproducible
@@ -197,8 +199,6 @@ cat(paste("    [Test] ",
           ".\n", sep = ""))
 
 
-
-
 # A few more features
 # ----------------------------------------------------------------
 regList <- c("ipc_ocat", "b_automat", "b_output", "b_signal", "b_execut", "b_inform", 
@@ -256,9 +256,117 @@ cat(paste("    [Test] ",
           ".\n", sep = ""))
 
 
+# Many features from different patent parts
+# ----------------------------------------------------------------
+regList <- c("ipc_ocat", "b_automat", "b_output", "b_signal", "b_execut", "b_inform", 
+             "b_input", "b_detect", "b_user", "b_display", "b_sensor", 
+             "b_switch", "b_retriev", "t_system", "t_apparatus", "t_automat",
+             "t_control", "a_system", "a_control", "a_data", "a_oper")
+model <- glm(paste("manAutomat ~ ", paste(regList, collapse = " + "), sep = ""),
+             family = binomial(link = 'logit'), data = train)
+summary(model)
+
+trueClass <- factor(ifelse(train$manAutomat == 1, "yes", "no"))
+trueClass <- relevel(trueClass, "yes")
+
+predAutomat <- predict(model, 
+                       newdata = subset(train, select = c("manAutomat", regList)), 
+                       type='response')
+predClass <- factor(ifelse(predAutomat > 0.5, "yes", "no"))
+predClass <- relevel(predClass, "yes")
+
+# Calculate classification statistics
+cm <- confusionMatrix(predClass, trueClass, dnn = c('predicted', 'true'))
+prec <- precision(predClass, trueClass)
+rec <- recall(predClass, trueClass)
+f1 <- F_meas(predClass, trueClass, beta = 1)
+
+cat('__________________\nIPC + some t_*, some a_* and many b_*:\n')
+cat(paste("[Training] ",
+          "F1-measure: ", round(f1, digits = 3), 
+          ", Precision: ", round(prec, digits = 2),
+          ", Recall: ", round(rec, digits = 2),
+          ", Accuracy: ", round(unname(cm$overall["Accuracy"]), digits = 2), 
+          ", N: ", length(trueClass), 
+          ".\n", sep = ""))
+
+trueClass <- factor(ifelse(test$manAutomat == 1, "yes", "no"))
+trueClass <- relevel(trueClass, "yes")
+
+predAutomat <- predict(model, 
+                       newdata = subset(test, select = c("manAutomat", regList)), 
+                       type='response')
+predClass <- factor(ifelse(predAutomat > 0.5, "yes", "no"))
+predClass <- relevel(predClass, "yes")
+
+# Calculate classification statistics
+cm <- confusionMatrix(predClass, trueClass, dnn = c('predicted', 'true'))
+prec <- precision(predClass, trueClass)
+rec <- recall(predClass, trueClass)
+f1 <- F_meas(predClass, trueClass, beta = 1)
+
+cat(paste("    [Test] ",
+          "F1-measure: ", round(f1, digits = 3), 
+          ", Precision: ", round(prec, digits = 2),
+          ", Recall: ", round(rec, digits = 2),
+          ", Accuracy: ", round(unname(cm$overall["Accuracy"]), digits = 2), 
+          ", N: ", length(trueClass), 
+          ".\n", sep = ""))
 
 
+# Many features from different patent parts
+# ----------------------------------------------------------------
+regList <- c("logPatLen", "b_automat")
+model <- glm(paste("manAutomat ~ ", paste(regList, collapse = " + "), sep = ""),
+             family = binomial(link = 'logit'), data = train)
+summary(model)
 
+trueClass <- factor(ifelse(train$manAutomat == 1, "yes", "no"))
+trueClass <- relevel(trueClass, "yes")
+
+predAutomat <- predict(model, 
+                       newdata = subset(train, select = c("manAutomat", regList)), 
+                       type='response')
+predClass <- factor(ifelse(predAutomat > 0.5, "yes", "no"))
+predClass <- relevel(predClass, "yes")
+
+# Calculate classification statistics
+cm <- confusionMatrix(predClass, trueClass, dnn = c('predicted', 'true'))
+prec <- precision(predClass, trueClass)
+rec <- recall(predClass, trueClass)
+f1 <- F_meas(predClass, trueClass, beta = 1)
+
+cat('__________________\nLog(patent lenght) + b_automat:\n')
+cat(paste("[Training] ",
+          "F1-measure: ", round(f1, digits = 3), 
+          ", Precision: ", round(prec, digits = 2),
+          ", Recall: ", round(rec, digits = 2),
+          ", Accuracy: ", round(unname(cm$overall["Accuracy"]), digits = 2), 
+          ", N: ", length(trueClass), 
+          ".\n", sep = ""))
+
+trueClass <- factor(ifelse(test$manAutomat == 1, "yes", "no"))
+trueClass <- relevel(trueClass, "yes")
+
+predAutomat <- predict(model, 
+                       newdata = subset(test, select = c("manAutomat", regList)), 
+                       type='response')
+predClass <- factor(ifelse(predAutomat > 0.5, "yes", "no"))
+predClass <- relevel(predClass, "yes")
+
+# Calculate classification statistics
+cm <- confusionMatrix(predClass, trueClass, dnn = c('predicted', 'true'))
+prec <- precision(predClass, trueClass)
+rec <- recall(predClass, trueClass)
+f1 <- F_meas(predClass, trueClass, beta = 1)
+
+cat(paste("    [Test] ",
+          "F1-measure: ", round(f1, digits = 3), 
+          ", Precision: ", round(prec, digits = 2),
+          ", Recall: ", round(rec, digits = 2),
+          ", Accuracy: ", round(unname(cm$overall["Accuracy"]), digits = 2), 
+          ", N: ", length(trueClass), 
+          ".\n", sep = ""))
 
 
 
