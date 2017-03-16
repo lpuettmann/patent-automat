911b33f6bf0ec9c12513e2829d2b93b967b788d8
lpuettmann <lukas.puettmann@uni-bonn.de>
2017-03-16 07:55:56 +0100

    ..

--- R/regular_models.R
+++ R/regular_models.R
@@ -39,74 +39,19 @@ xTrain <- data.matrix(select(train, -manAutomat))
 yTest <- data.matrix(select(test, manAutomat))
 xTest <- data.matrix(select(test, -manAutomat))
 
-fitLasso <- glmnet(xTrain, yTrain, family = "gaussian", alpha = 1)
-fitRidge <- glmnet(xTrain, yTrain, family = "gaussian", alpha = 0)
-fitElnet <- glmnet(xTrain, yTrain, family = "gaussian", alpha = 0.5)
+fitLasso <- glmnet(xTrain, yTrain, family = "binomial", standardize = TRUE)
 
-
-# plot(fit, label = T)
-# print(fit)
-# coef(fit, s = 0.1)
 
 cat('Cross-validate ... '); tic = proc.time()[3]
-pb <- progress_estimated(11)
-for (i in 0:10) {
-  assign(paste("fit", i, sep=""), cv.glmnet(xTrain, yTrain, type.measure="mse", alpha=i/10, family="gaussian"))
-  pb$tick()$print()
-}
+cvfit = cv.glmnet(xTrain, yTrain, family = "binomial", standardize = TRUE)
 cat(paste("done. [", round(proc.time()[3] - tic, digits = 1), "s]\n", sep = ""))
 
+plot(cvfit)
+cvfit$lambda.min
+cvfit$lambda.1se
 
-# Plot solution paths:
-par(mfrow=c(3,2))
-
-# For plotting options, type '?plot.glmnet' in R console
-plot(fitLasso, xvar="lambda")
-plot(fit10, main="LASSO")
-
-plot(fitRidge, xvar="lambda")
-plot(fit0, main="Ridge")
-
-plot(fitElnet, xvar="lambda")
-plot(fit5, main="Elastic Net")
-
-yhat0 <- predict(fit0, s=fit0$lambda.1se, newx=xTest)
-yhat1 <- predict(fit1, s=fit1$lambda.1se, newx=xTest)
-yhat2 <- predict(fit2, s=fit2$lambda.1se, newx=xTest)
-yhat3 <- predict(fit3, s=fit3$lambda.1se, newx=xTest)
-yhat4 <- predict(fit4, s=fit4$lambda.1se, newx=xTest)
-yhat5 <- predict(fit5, s=fit5$lambda.1se, newx=xTest)
-yhat6 <- predict(fit6, s=fit6$lambda.1se, newx=xTest)
-yhat7 <- predict(fit7, s=fit7$lambda.1se, newx=xTest)
-yhat8 <- predict(fit8, s=fit8$lambda.1se, newx=xTest)
-yhat9 <- predict(fit9, s=fit9$lambda.1se, newx=xTest)
-yhat10 <- predict(fit10, s=fit10$lambda.1se, newx=xTest)
-
-mse0 <- mean((yTest - yhat0)^2)
-mse1 <- mean((yTest - yhat1)^2)
-mse2 <- mean((yTest - yhat2)^2)
-mse3 <- mean((yTest - yhat3)^2)
-mse4 <- mean((yTest - yhat4)^2)
-mse5 <- mean((yTest - yhat5)^2)
-mse6 <- mean((yTest - yhat6)^2)
-mse7 <- mean((yTest - yhat7)^2)
-mse8 <- mean((yTest - yhat8)^2)
-mse9 <- mean((yTest - yhat9)^2)
-mse10 <- mean((yTest - yhat10)^2)
-
-minErr <- c(mse0, mse1, mse2, mse3, mse4, mse5, mse6, mse7, mse8, mse9, mse10)
-
-# cat('Cross-validate ... '); tic = proc.time()[3]
-# cvfit = cv.glmnet(x, y, family = "binomial")
-# cat(paste("done. [", round(proc.time()[3] - tic, digits = 1), "s]\n", sep = ""))
-# 
-# plot(cvfit)
-# cvfit$lambda.min
-# cvfit$lambda.1se
-# 
-# coef(cvfit, s = "lambda.min")
-# 
-# predict(cvfit, newx = xTest, s = "lambda.min")
+coef(cvfit, s = "lambda.min")
+predict(cvfit, newx = xTest, s = "lambda.min")
 
 
 
