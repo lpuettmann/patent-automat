204e2b818ca2225e41d60947eea273732dc35e7c
lpuettmann <lukas.puettmann@uni-bonn.de>
2017-08-26 11:13:10 +0200

    yesterday's changes to the matching

--- conversion_ipc2sic/construct_sic_automix.m
+++ conversion_ipc2sic/construct_sic_automix.m
@@ -21,14 +21,25 @@ for t=1:length(year_start:year_end)
     % Indicators for patents showing if they are classified as automation
     % patents.
     automPat = patsearch_results.is_nbAutomat;
+    
+    % Get indicator for patent being software or robot
+    ix_software = +(patsearch_results.is_nbAutomat & ...
+                 (patsearch_results.overcat_classnr >= 21) & ...
+                 (patsearch_results.overcat_classnr <= 24));
+    ix_robot = +(patsearch_results.is_nbAutomat & ...
+              (patsearch_results.overcat_classnr >= 41) & ...
+              (patsearch_results.overcat_classnr <= 59)); 
 
     % List with IPC technology classification numbers
     ipc_list = patsearch_results.classnr_ipc;
-    [frac_counts, automPat_flatten] = make_frac_count(ipc_list, automPat);
+    [frac_counts, automPat_flatten, ix_software_flatten, ix_robot_flatten] = ...
+        make_frac_count(ipc_list, automPat, ix_software, ix_robot);
     
     ipc_flat = flatten_cellarray(ipc_list);    
     
     assert( length(frac_counts) == length(ipc_flat) )
+    assert( length(automPat_flatten) == length(ix_software_flatten))
+    assert( length(automPat_flatten) == length(ix_robot_flatten))
     
     % Only keep first 4 characters of IPC number
     ipc_short = shorten_cellarray(ipc_flat, 4);
@@ -43,35 +54,37 @@ for t=1:length(year_start:year_end)
         usefrq = ipcsicfinalv5.usefrq;
         
         sic_automix = match_sic2ipc(ipc_concordance, ipc_short, ...
-            frac_counts, automPat_flatten, ix_pick, mfgfrq, usefrq);
+            frac_counts, automPat_flatten, ix_software_flatten, ...
+            ix_robot_flatten, ix_pick, mfgfrq, usefrq);
                
         
         % Get summary for all IPCs mapping into SICs
         sic_automix_yres.sic(ix_sic, 1) = str2num( sic_pick );
         
-        sic_automix_yres.nr_pat(ix_sic, 1) = ...
-            sum( sic_automix.total_nr_matched );
+        sic_automix_yres.nr_pat(ix_sic, 1) = sum( sic_automix.total_nr_matched );
+        sic_automix_yres.nr_fracpat(ix_sic, 1) = sum( sic_automix.total_frac_counts );
         
-        sic_automix_yres.nr_fracpat(ix_sic, 1) = ...
-            sum( sic_automix.total_frac_counts );
+        sic_automix_yres.nr_autompat(ix_sic, 1) = sum( sic_automix.autompat_nr_matched );
+        sic_automix_yres.nr_fracautompat(ix_sic, 1) = sum( sic_automix.autompat_frac_counts );  
         
-        sic_automix_yres.nr_autompat(ix_sic, 1) = ...
-            sum( sic_automix.autompat_nr_matched );
+        sic_automix_yres.nr_software(ix_sic, 1) = sum( sic_automix.software_nr_matched );
+        sic_automix_yres.nr_fracsoftware(ix_sic, 1) = sum( sic_automix.software_frac_counts );  
         
-        sic_automix_yres.nr_fracautompat(ix_sic, 1) = ...
-            sum( sic_automix.autompat_frac_counts );  
+        sic_automix_yres.nr_robot(ix_sic, 1) = sum( sic_automix.robot_nr_matched );
+        sic_automix_yres.nr_fracrobot(ix_sic, 1) = sum( sic_automix.robot_frac_counts );  
         
-        sic_automix_yres.patents_mfg(ix_sic, 1) = ...
-            sum( sic_automix.patents_mfg );  
+        sic_automix_yres.patents_mfg(ix_sic, 1) = sum( sic_automix.patents_mfg );  
+        sic_automix_yres.patents_use(ix_sic, 1) = sum( sic_automix.patents_use );  
         
-        sic_automix_yres.patents_use(ix_sic, 1) = ...
-            sum( sic_automix.patents_use );  
+        sic_automix_yres.automix_mfgt(ix_sic, 1) = sum( sic_automix.automix_mfg );  
+        sic_automix_yres.automix_use(ix_sic, 1) = sum( sic_automix.automix_use );  
         
-        sic_automix_yres.automix_mfgt(ix_sic, 1) = ...
-            sum( sic_automix.automix_mfg );  
-        
-        sic_automix_yres.automix_use(ix_sic, 1) = ...
-            sum( sic_automix.automix_use );  
+        sic_automix_yres.software_mfgt(ix_sic, 1) = sum( sic_automix.software_mfg );  
+        sic_automix_yres.software_use(ix_sic, 1) = sum( sic_automix.software_use );  
+                
+        sic_automix_yres.robot_mfgt(ix_sic, 1) = sum( sic_automix.robot_mfg );  
+        sic_automix_yres.robot_use(ix_sic, 1) = sum( sic_automix.robot_use );  
+
 
         % Print a progress message for every 100th industry and the last
         % one.
@@ -87,9 +100,8 @@ for t=1:length(year_start:year_end)
 
     savename = horzcat('sic_automix/sic_automix_yres_', ...
         num2str(ix_year), '.mat');
-%     save(savename, 'sic_automix_yres'); 
-%     fprintf('Saved: %s.\n', savename)
-    fprintf('Finished (not saved): %s.\n', savename)
-    
+    save(savename, 'sic_automix_yres'); 
+    fprintf('Saved: %s.\n', savename)
+
     clear sic_automix_yres
 end
--- conversion_ipc2sic/match_sic2ipc.m
+++ conversion_ipc2sic/match_sic2ipc.m
@@ -1,5 +1,6 @@
 function sic_automix = match_sic2ipc(ipc_concordance, ipc_short, ...
-    frac_counts, automPat_flatten, ix_pick, mfgfrq, usefrq)   
+    frac_counts, automPat_flatten, ix_software_flatten, ...
+            ix_robot_flatten, ix_pick, mfgfrq, usefrq)
 
 
 %% Check inputs
@@ -21,7 +22,7 @@ for ix_ipc=1:length(ipc_concordance)
     pick_ipc = ipc_concordance{ix_ipc};
 
     % Find patents with the right IPCs
-    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    % -------------------------------------------------------
     ix_ipc_match = strcmp(ipc_short, pick_ipc);
 
     % Number of patent IPCs that fit
@@ -31,12 +32,12 @@ for ix_ipc=1:length(ipc_concordance)
     match_frac_counts = frac_counts(ix_ipc_match);
 
     % Get total fractional count of patents with the right IPC
-    sic_automix.total_frac_counts(ix_ipc,1) = sum( ...
-        match_frac_counts );
+    sic_automix.total_frac_counts(ix_ipc,1) = sum(match_frac_counts);
 
-    % Only count automation patents
+    % Automation
+    % ~~~~~~~~~~~~~~~
     autompat_match = ix_ipc_match .* automPat_flatten;
-
+    
     % Number of automation patents match from IPC to SIC
     sic_automix.autompat_nr_matched(ix_ipc,1) = sum( autompat_match ); 
 
@@ -46,26 +47,82 @@ for ix_ipc=1:length(ipc_concordance)
     % Get total count of fractional automation patents
     sic_automix.autompat_frac_counts(ix_ipc,1) = sum( ...
         autompat_matched_frac_counts );
+    
+    % Software
+    % ~~~~~~~~~~~~~~~
+    software_match = ix_ipc_match .* ix_software_flatten;
+    
+    % Number of automation patents match from IPC to SIC
+    sic_automix.software_nr_matched(ix_ipc,1) = sum( software_match ); 
+
+    % Get fractional count of patents with the right IPC
+    software_matched_frac_counts = frac_counts( find( software_match ) );
+
+    % Get total count of fractional automation patents
+    sic_automix.software_frac_counts(ix_ipc,1) = sum( ...
+        software_matched_frac_counts );
+    
+    % Robots
+    % ~~~~~~~~~~~~~~~
+    robot_match = ix_ipc_match .* ix_robot_flatten;
+    
+    % Number of automation patents match from IPC to SIC
+    sic_automix.robot_nr_matched(ix_ipc,1) = sum( robot_match ); 
+
+    % Get fractional count of patents with the right IPC
+    robot_matched_frac_counts = frac_counts( find( robot_match ) );
+
+    % Get total count of fractional automation patents
+    sic_automix.robot_frac_counts(ix_ipc,1) = sum( ...
+        robot_matched_frac_counts );
+    
 
     % Extract empirical frequencies for this IPC
-    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    % -------------------------------------------------------
 
-    % Industries of manufacture (all patents)
+    % All patents
+    % ~~~~~~~~~~~~~~~
+    
+    % Industries of manufacture
     sic_automix.patents_mfg(ix_ipc,1) = mfgfrq( ix_pick( ix_ipc ) ) * ...
         sic_automix.total_frac_counts( ix_ipc, 1 );
 
-    % Sector of use (all patents)
+    % Sector of use
     sic_automix.patents_use(ix_ipc,1) = usefrq( ix_pick( ix_ipc ) ) * ...
         sic_automix.total_frac_counts( ix_ipc, 1 );
     
+    % Automation
+    % ~~~~~~~~~~~~~~~
     
-    % Industries of manufacture (automation patents)
+    % Industries of manufacture
     sic_automix.automix_mfg(ix_ipc,1) = mfgfrq( ix_pick( ix_ipc ) ) * ...
         sic_automix.autompat_frac_counts( ix_ipc, 1 );
 
-    % Sector of use (automation patents)
+    % Sector of use
     sic_automix.automix_use(ix_ipc,1) = usefrq( ix_pick( ix_ipc ) ) * ...
         sic_automix.autompat_frac_counts( ix_ipc, 1 ); 
+    
+    % Software
+    % ~~~~~~~~~~~~~~~
+
+    % Industries of manufacture
+    sic_automix.software_mfg(ix_ipc,1) = mfgfrq( ix_pick( ix_ipc ) ) * ...
+        sic_automix.software_frac_counts( ix_ipc, 1 );
+
+    % Sector of use
+    sic_automix.software_use(ix_ipc,1) = usefrq( ix_pick( ix_ipc ) ) * ...
+        sic_automix.software_frac_counts( ix_ipc, 1 ); 
+    
+    % Robots
+    % ~~~~~~~~~~~~~~~
+    
+    % Industries of manufacture
+    sic_automix.robot_mfg(ix_ipc,1) = mfgfrq( ix_pick( ix_ipc ) ) * ...
+        sic_automix.robot_frac_counts( ix_ipc, 1 );
+
+    % Sector of use
+    sic_automix.robot_use(ix_ipc,1) = usefrq( ix_pick( ix_ipc ) ) * ...
+        sic_automix.robot_frac_counts( ix_ipc, 1 ); 
 end
 
 
