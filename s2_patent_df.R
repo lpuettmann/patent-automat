880532669019452b340a9165ce2d74dd22f7f2b8
lpuettmann <lukas.puettmann@uni-bonn.de>
2017-08-30 10:10:14 +0200

    analyze individual patent data

--- s2_patent_df.R
+++ s2_patent_df.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(R.matlab)
 
 # Concatenate data on yearly patents --------------------------------------
-df <- list() # Initialize empty data frame
+patent_df <- list() # Initialize empty data frame
 
 for (tper in 1976:2014) {
   
@@ -14,21 +14,21 @@ for (tper in 1976:2014) {
   varNames <- names(matlabFile$patent.info[,,1])
   datList = matlabFile$patent.info
   datList = lapply(datList, unlist, use.names=FALSE)
-  patent_df <- as.data.frame(datList)
-  names(patent_df) <- varNames
+  year_df <- as.data.frame(datList)
+  names(year_df) <- varNames
   
   # R likes to replace . with _ in variable names, so revert that
-  names(patent_df) <- gsub("\\.", "_", names(patent_df))
+  names(year_df) <- gsub("\\.", "_", names(year_df))
   
-  patent_df$year <- tper
+  year_df$year <- tper
   
   # Append
-  df <- bind_rows(df, patent_df) t
+  patent_df <- bind_rows(patent_df, year_df) t
   
   cat(paste0("done. [", round(proc.time()[3] - tic, digits = 1), "s]\n"))
 }
 
-df <- df %>% 
+patent_df <- patent_df %>% 
   select(year, 
          week, 
          patentnr, 
@@ -38,13 +38,13 @@ df <- df %>%
          overcat_classnr, 
          length_pattext) 
 
-df$overcat_classnr[(df$overcat_classnr == "NaN")] <- NA
+patent_df$overcat_classnr[(patent_df$overcat_classnr == "NaN")] <- NA
 
-df <- df %>% 
+patent_df <- patent_df %>% 
   mutate(overcat_classnr = as.numeric(overcat_classnr),
          patentnr = as.character(patentnr),
          uspc_primary = as.character(uspc_primary))
 
 # Save --------------------------------------------------------------------
-write_rds(df, "output/patent_df.rds")
+write_rds(patent_df, "output/year_df.rds")
 
--- s3_analyze_patents.R
+++ s3_analyze_patents.R
@@ -1,12 +1,28 @@
 
 library(tidyverse)
+library(ggthemes)
+library(readstata13)
 
 df <- read_rds("output/patent_df.rds")
 
 df_yrly <- df %>% 
   group_by(year) %>% 
   summarize(nr_patents = n(),
-            nr_automat = sum(automat))
+            nr_automat = sum(automat)) %>% 
+  mutate(sh_automat = nr_automat / nr_patents)
 
+df_automat = subset(df, automat == 1)
 
+ggplot() + 
+  geom_hline(yintercept = 0, size = 0.3, color = "grey80") +
+  geom_line(data = df, aes(log(length_pattext)), color = "#252525", stat="density") +
+  geom_line(data = df_automat, aes(log(length_pattext)), color = "#969696", stat="density") +
+  theme_tufte(base_family="Palatino", ticks=FALSE)
+  
+mean(log(df$length_pattext))
+mean(log(df_automat$length_pattext))
+
+save.dta13(data = df, 
+           file = "output/patent_data.dta",
+           version = 13)
 
